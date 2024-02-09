3576816e942e83a5f56cc8a1da816ed048dd0ffd
Daniel Chiba <danchiba@vt.edu>
2024-02-09 18:57:37 -0500

    Platform specific directories
    Moved architecture specific code/Makefiles and refactored
    Makefile hierarchy to make it easier for new architectures to
    be added.

--- Makefile.mk
+++ Makefile.mk
@@ -1,37 +1,6 @@
-TC_PATH = /usr
-TOOLCHAIN = arm-none-eabi
-ARMGNU = $(TC_PATH)/bin/$(TOOLCHAIN)
-CC = $ arm-none-eabi-gcc
-CHIP = cortex-m0plus
-OPTLVL = -O3 
+ifeq (,$(findstring $(ARCH), arm))
+$(error Usage: make ARCH=arm)
+endif
 
-#FLAGS = -DBARE_METAL -Wall $(OPTLVL) -target $(TOOLCHAIN) -mcpu=$(CHIP) -mthumb --specs=nosys.specs -nostartfiles -ffreestanding -std=c99 -fomit-frame-pointer -fno-optimize-sibling-calls #-static
-FLAGS = -DBARE_METAL -Wall $(OPTLVL) -march=armv6-m -mcpu=$(CHIP) -mthumb -msoft-float --specs=nosys.specs -nostartfiles -ffreestanding -std=c99 -fomit-frame-pointer -fno-optimize-sibling-calls 
-#LIBS = --start-group -lm -lc -lbuiltins --end-group
-LIBS = -lnosys -lc -lm
-OBJS := vectors.o putget.o supportFuncs.o LED.o $(OBJS)
-#LINKDIR=-L$(TC_PATH)/$(TOOLCHAIN)/lib
-LINKDIR=
-INCLIB = -I$(TC_PATH)/$(TOOLCHAIN)/include 
-
-all: main.elf 
-
-%.o: %.s
-	$(CC) $(FLAGS) $(INCLIB) -c -o $@ $<
-
-%.o: ../%.s
-	$(CC) $(FLAGS) $(INCLIB) -c -o $@ $<
-
-%.o: %.c
-	$(CC) ${FLAGS} $(INCLIB) -c -o $@ $< 
-
-%.o: ../%.c
-	$(CC) ${FLAGS} $(INCLIB) -c -o $@ $< 
-
-main.elf: $(OBJS) ../vectors.s ../putget.s ../supportFuncs.c ../LED.s
-	$(CC) $(FLAGS) -u _printf_float -T ../memmap $(LINKDIR) $(OBJS) -o main.elf $(LIBS)
-	$(TOOLCHAIN)-objdump -d main.elf > main.lst
-	$(TOOLCHAIN)-objcopy main.elf main.bin -O binary
-
-clean: more_clean
-	rm -rf *.o *.elf output* *.lst *.bin *~
+ARCH_DIR=../$(ARCH)
+include $(ARCH_DIR)/Makefile.mk
