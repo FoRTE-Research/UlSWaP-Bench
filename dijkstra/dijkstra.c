ace3c75ccdde48204e97ddb20cccb881249edbe7
Daniel Chiba <danchiba@vt.edu>
2024-03-18 18:36:06 -0400

    Fix dijkstra
    Integrated fixes from 678b7ac in MiBench2.
    Re-enabled printing instead of LED operations.
    Added test reference output.

--- dijkstra/dijkstra.c
+++ dijkstra/dijkstra.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "../bareBench.h"
 #include "input.h"
@@ -21,8 +22,20 @@ struct _QITEM
 };
 typedef struct _QITEM QITEM;
 
-QITEM allocated[100];
+/*
+ * Dijkstra's algorithm calculates the shortest path between two nodes
+ * by calculating the shortest path from 1 node to all the other nodes.
+ * At the Nth step, the algorithm "visits" the closest node to
+ * any of the N already visited nodes.
+ * Then, the algorithm checks the (NUM_NODES - N) unvisited nodes and
+ * adds a node to the queue if a shorter path to that node is found.
+ * Thus, the maximum number of nodes that could be added to the queue is
+ * NUM_NODES + (NUM_NODES - 1) + ... + 2 + 1 ~= (NUM_NODES * NUM_NODES / 2)
+ */
+#define ARRAY_SIZE (NUM_NODES * NUM_NODES / 2)
+QITEM allocated[ARRAY_SIZE];
 QITEM *qHead = NULL;
+int notAll = 0;
 
 int g_qCount = 0;
 NODE rgnNodes[NUM_NODES];
@@ -37,20 +50,19 @@ void print_path (NODE *rgnNodes, int chNode)
     {
       print_path(rgnNodes, rgnNodes[chNode].iPrev);
     }
-  //printf (" %d", chNode);
-  fflush(stdout);
+  printf (" %d", chNode);
+//   fflush(stdout);
 }
 
 void enqueue (int iNode, int iDist, int iPrev)
 {
-  static int notAll = 0;
   QITEM *qNew = &allocated[notAll];
   notAll++;
   QITEM *qLast = qHead;
   
-  if (!qNew) 
+  if (notAll >= ARRAY_SIZE) 
     {
-      //printf("Out of memory.\n");
+      printf("Out of memory.\r\n");
       exit(1);
     }
   qNew->iNode = iNode;
@@ -96,7 +108,7 @@ int qcount (void)
 
 int dijkstra(int chStart, int chEnd) 
 {
-  
+  notAll = 0;
 
   for (ch = 0; ch < NUM_NODES; ch++)
     {
@@ -105,7 +117,7 @@ int dijkstra(int chStart, int chEnd)
     }
   if (chStart == chEnd) 
     {
-      //printf("Shortest path is 0 in cost. Just stay where you are.\n");
+      printf("Shortest path is 0 in cost. Just stay where you are.\r\n");
     }
   else
     {
@@ -131,18 +143,18 @@ int dijkstra(int chStart, int chEnd)
 	    }
 	}
       
-      //printf("Shortest path is %d in cost. ", rgnNodes[chEnd].iDist);
-      //printf("Path is: ");
-      //print_path(rgnNodes, chEnd);
-      //printf("\n");
+      printf("Shortest path is %d in cost. ", rgnNodes[chEnd].iDist);
+      printf("Path is: ");
+      print_path(rgnNodes, chEnd);
+      printf("\r\n");
     }
     return 0;
 }
 
 int main(int argc, char *argv[]) {
   int i,j;
-  initLED();
-  LED(1);
+//   initLED();
+//   LED(1);
    /* make a fully connected matrix */
    // see input.h
   /* finds 10 shortest paths between nodes */
@@ -150,6 +162,6 @@ int main(int argc, char *argv[]) {
 			j=j%NUM_NODES;
       dijkstra(i,j);
   }
-  LED(1);
+//   LED(1);
   return 0;
 }
