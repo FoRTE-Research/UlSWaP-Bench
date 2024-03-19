faa0ccd8bdda6193f83a64135641fa1b4bbc3ba3
Daniel Chiba <danchiba@vt.edu>
2024-03-18 20:20:56 -0400

    Fix blowfish
    Switch to stdint types.
    Reduce input size.
    Update test reference output.

--- blowfish/blowfish_test.c
+++ blowfish/blowfish_test.c
@@ -23,44 +23,48 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 unsigned char KEY[] = "1234567890abcdeffedcba0987654321";
 
-int main(void) {
-  unsigned long L = 1, R = 2;
-  BLOWFISH_CTX ctx;
+int main(void)
+{
+    uint32_t L = 1, R = 2;
+    printf("size = %zu\r\n", sizeof(L));
+    BLOWFISH_CTX ctx;
 
-  Blowfish_Init (&ctx, (unsigned char*)"TESTKEY", 7);
+    Blowfish_Init(&ctx, (unsigned char *)"TESTKEY", 7);
 
-  Blowfish_Encrypt(&ctx, &L, &R);
-  printf("%08lX %08lX\n", L, R);
-  if (L == 0xDF333FD2L && R == 0x30A71BB4L)
-	  printf("Test encryption OK.\n");
-  else
-	  printf("Test encryption failed.\n");
+    Blowfish_Encrypt(&ctx, &L, &R);
+    printf("%08X %08X\r\n", L, R);
+    if (L == 0xDF333FD2L && R == 0x30A71BB4L)
+        printf("Test encryption OK.\r\n");
+    else
+        printf("Test encryption failed.\r\n");
 
-  Blowfish_Decrypt(&ctx, &L, &R);
-  if (L == 1 && R == 2)
-  	  printf("Test decryption OK.\n");
-  else
-	  printf("Test decryption failed.\n");
-    
-  printf("Encrypt message\n");
-  Blowfish_Init (&ctx, KEY, sizeof(KEY));
+    Blowfish_Decrypt(&ctx, &L, &R);
+    if (L == 1 && R == 2)
+        printf("Test decryption OK.\r\n");
+    else
+        printf("Test decryption failed.\r\n");
 
-  unsigned long * plaintextPtr = (unsigned long *)test_data;
-  while(plaintextPtr < (unsigned long *)(test_data + sizeof(test_data))) {
-      Blowfish_Encrypt(&ctx, &plaintextPtr[0], &plaintextPtr[1]);
-      printf("%08lX %08lX\n", plaintextPtr[0], plaintextPtr[1]);
-      plaintextPtr += 2;
-  }
+    printf("Encrypt message\r\n");
+    Blowfish_Init(&ctx, KEY, sizeof(KEY));
 
-  printf("Decrypt message\n");
-  Blowfish_Init (&ctx, KEY, sizeof(KEY));
-    
-  plaintextPtr = (unsigned long *)test_data;
-  while(plaintextPtr < (unsigned long *)(test_data + sizeof(test_data))) {
-      Blowfish_Decrypt(&ctx, &plaintextPtr[0], &plaintextPtr[1]);
-      printf("%08lX %08lX\n", plaintextPtr[0], plaintextPtr[1]);
-      plaintextPtr += 2;
-  }
-    
-  return 0;
+    uint32_t *plaintextPtr = (uint32_t *)test_data;
+    while (plaintextPtr < (uint32_t *)(test_data + sizeof(test_data)))
+    {
+        Blowfish_Encrypt(&ctx, &plaintextPtr[0], &plaintextPtr[1]);
+        printf("%08X %08X\r\n", plaintextPtr[0], plaintextPtr[1]);
+        plaintextPtr += 2;
+    }
+
+    printf("Decrypt message\r\n");
+    Blowfish_Init(&ctx, KEY, sizeof(KEY));
+
+    plaintextPtr = (uint32_t *)test_data;
+    while (plaintextPtr < (uint32_t *)(test_data + sizeof(test_data)))
+    {
+        Blowfish_Decrypt(&ctx, &plaintextPtr[0], &plaintextPtr[1]);
+        printf("%08X %08X\r\n", plaintextPtr[0], plaintextPtr[1]);
+        plaintextPtr += 2;
+    }
+
+    return 0;
 }
