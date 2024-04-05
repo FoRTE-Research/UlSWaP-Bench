247fbf4ff38d83536710e60958497522acc76879
Daniel Chiba <danchiba@vt.edu>
2024-04-05 17:21:13 -0400

    Cleanup FFT
    Switched all types to stdint types.
    Removed redundant code.
    Better formatting.

--- fft/fourierf.c
+++ fft/fourierf.c
@@ -24,59 +24,58 @@
 #include "fourier.h"
 #include "ddcmath.h"
 
-#define CHECKPOINTER(p)  CheckPointer(p,#p)
+#define CHECKPOINTER(p) CheckPointer(p, #p)
 
-static void CheckPointer ( void *p, char *name )
+static void CheckPointer(void *p, char *name)
 {
-    if ( p == NULL )
+    if (p == NULL)
     {
-        fprintf ( stderr, "Error in fft_float():  %s == NULL\r\n", name );
+        fprintf(stderr, "Error in fft_float():  %s == NULL\r\n", name);
         exit(1);
     }
 }
 
-
-void fft_float (
-    unsigned  NumSamples,
-    int       InverseTransform,
+void fft_float(
+    uint32_t NumSamples,
+    int32_t  InverseTransform,
     float    *RealIn,
     float    *ImagIn,
     float    *RealOut,
-    float    *ImagOut )
+    float    *ImagOut)
 {
-    unsigned NumBits;    /* Number of bits needed to store indices */
-    unsigned i, j, k, n;
-    unsigned BlockSize, BlockEnd;
+    uint32_t NumBits; /* Number of bits needed to store indices */
+    uint32_t i, j, k, n;
+    uint32_t BlockSize, BlockEnd;
 
     double angle_numerator = 2.0 * DDC_PI;
-    double tr, ti;     /* temp real, temp imaginary */
+    double tr, ti; /* temp real, temp imaginary */
 
-    if ( !IsPowerOfTwo(NumSamples) )
+    if (!IsPowerOfTwo(NumSamples))
     {
-        fprintf (
+        fprintf(
             stderr,
             "Error in fft():  NumSamples=%u is not power of two\r\n",
-            NumSamples );
+            NumSamples);
 
         exit(1);
     }
 
-    if ( InverseTransform )
+    if (InverseTransform)
         angle_numerator = -angle_numerator;
 
-    CHECKPOINTER ( RealIn );
-    CHECKPOINTER ( RealOut );
-    CHECKPOINTER ( ImagOut );
+    CHECKPOINTER(RealIn);
+    CHECKPOINTER(RealOut);
+    CHECKPOINTER(ImagOut);
 
-    NumBits = NumberOfBitsNeeded ( NumSamples );
+    NumBits = NumberOfBitsNeeded(NumSamples);
 
     /*
     **   Do simultaneous data copy and bit-reversal ordering into outputs...
     */
 
-    for ( i=0; i < NumSamples; i++ )
+    for (i = 0; i < NumSamples; i++)
     {
-        j = ReverseBits ( i, NumBits );
+        j = ReverseBits(i, NumBits);
         RealOut[j] = RealIn[i];
         ImagOut[j] = (ImagIn == NULL) ? 0.0 : ImagIn[i];
     }
@@ -86,17 +85,17 @@ void fft_float (
     */
 
     BlockEnd = 1;
-    for ( BlockSize = 2; BlockSize <= NumSamples; BlockSize <<= 1 )
+    for (BlockSize = 2; BlockSize <= NumSamples; BlockSize <<= 1)
     {
         double delta_angle = angle_numerator / (double)BlockSize;
-        double sm2 = sin ( -2 * delta_angle );
-        double sm1 = sin ( -delta_angle );
-        double cm2 = cos ( -2 * delta_angle );
-        double cm1 = cos ( -delta_angle );
+        double sm2 = sin(-2 * delta_angle);
+        double sm1 = sin(-delta_angle);
+        double cm2 = cos(-2 * delta_angle);
+        double cm1 = cos(-delta_angle);
         double w = 2 * cm1;
         double ar[3], ai[3];
 
-        for ( i=0; i < NumSamples; i += BlockSize )
+        for (i = 0; i < NumSamples; i += BlockSize)
         {
             ar[2] = cm2;
             ar[1] = cm1;
@@ -104,19 +103,19 @@ void fft_float (
             ai[2] = sm2;
             ai[1] = sm1;
 
-            for ( j=i, n=0; n < BlockEnd; j++, n++ )
+            for (j = i, n = 0; n < BlockEnd; j++, n++)
             {
-                ar[0] = w*ar[1] - ar[2];
+                ar[0] = w * ar[1] - ar[2];
                 ar[2] = ar[1];
                 ar[1] = ar[0];
 
-                ai[0] = w*ai[1] - ai[2];
+                ai[0] = w * ai[1] - ai[2];
                 ai[2] = ai[1];
                 ai[1] = ai[0];
 
                 k = j + BlockEnd;
-                tr = ar[0]*RealOut[k] - ai[0]*ImagOut[k];
-                ti = ar[0]*ImagOut[k] + ai[0]*RealOut[k];
+                tr = ar[0] * RealOut[k] - ai[0] * ImagOut[k];
+                ti = ar[0] * ImagOut[k] + ai[0] * RealOut[k];
 
                 RealOut[k] = RealOut[j] - tr;
                 ImagOut[k] = ImagOut[j] - ti;
@@ -133,17 +132,16 @@ void fft_float (
     **   Need to normalize if inverse transform...
     */
 
-    if ( InverseTransform )
+    if (InverseTransform)
     {
         double denom = (double)NumSamples;
 
-        for ( i=0; i < NumSamples; i++ )
+        for (i = 0; i < NumSamples; i++)
         {
             RealOut[i] /= denom;
             ImagOut[i] /= denom;
         }
     }
 }
-
 
 /*--- end of file fourierf.c ---*/
--- fft/main.c
+++ fft/main.c
@@ -2,17 +2,15 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdint.h>
+
 #include "fourier.h"
+#include "../common.h"
 #include "../bareBench.h"
 
-void my_srand(uint32_t new_seed);
-uint32_t my_rand(void);
+const uint32_t MAXSIZE = 256;       // small 4096, 8192 inverse, 512 for memory-limited systems
+const uint32_t MAXWAVES = 4;        // large has 8
+const uint32_t MAX_NUMS_PER_LINE = 8;
 
-
-int invfft = 0;
-const unsigned MAXSIZE = 256;	   // small 4096, 8192 inverse, 512 for memory-limited systems
-const unsigned MAXWAVES = 4; // large has 8
-const unsigned MAX_NUMS_PER_LINE = 16;
 static float realin[256];
 static float imagin[256];
 static float realout[256];
@@ -20,26 +18,13 @@ static float imagout[256];
 static float Coeff[4];
 static float Amp[4];
 
-int old_main();
 
-// main for benchmark purposes that does fft and inverse fft
-int main()
-{
-    invfft = 0;
-    // MAXSIZE = 128;
-    old_main();
-    invfft = 1;
-    // MAXSIZE = 256;
-    old_main();
-    return 0;
-}
-
-void printf_float_array(const char* name, float *array, unsigned size)
+void printf_float_array(const char* name, float *array, uint32_t size)
 {
     printf("%s:\r\n", name);
-    for (unsigned i = 0, p = 0; i < size; i++)
+    for (uint32_t i = 0, p = 0; i < size; i++)
     {
-        printf("%+010.4f     ", printf_float(array[i]));
+        printf("%+011.4f     ", printf_float(array[i]));
         if (p == MAX_NUMS_PER_LINE - 1)
         {
             printf("\r\n");
@@ -53,43 +38,34 @@ void printf_float_array(const char* name, float *array, unsigned size)
     printf("\r\n");
 }
 
-int old_main()
+int main(void)
 {
-    unsigned i, j;
-    float *RealIn;
-    float *ImagIn;
-    float *RealOut;
-    float *ImagOut;
-    float *coeff;
-    float *amp;
+    uint32_t i, j;
+    float    *RealIn;
+    float    *ImagIn;
+    float    *RealOut;
+    float    *ImagOut;
+    float    *coeff;
+    float    *amp;
 
     my_srand(1);
 
-    /* RealIn=(float*)malloc(sizeof(float)*MAXSIZE);
-     ImagIn=(float*)malloc(sizeof(float)*MAXSIZE);
-     RealOut=(float*)malloc(sizeof(float)*MAXSIZE);
-     ImagOut=(float*)malloc(sizeof(float)*MAXSIZE);
-     coeff=(float*)malloc(sizeof(float)*MAXWAVES);
-     amp=(float*)malloc(sizeof(float)*MAXWAVES);
-    */
-
     // Statically allocate
-    RealIn = realin;
-    ImagIn = imagin;
+    RealIn  = realin;
+    ImagIn  = imagin;
     RealOut = realout;
     ImagOut = imagout;
-    coeff = Coeff;
-    amp = Amp;
+    coeff   = Coeff;
+    amp     = Amp;
 
     /* Makes MAXWAVES waves of random amplitude and period */
     for (i = 0; i < MAXWAVES; i++)
     {
         coeff[i] = my_rand() % 1000;
-        amp[i] = my_rand() % 1000;
+        amp[i]   = my_rand() % 1000;
     }
     for (i = 0; i < MAXSIZE; i++)
     {
-        /*   RealIn[i]=my_rand();*/
         RealIn[i] = 0;
         for (j = 0; j < MAXWAVES; j++)
         {
@@ -109,18 +85,17 @@ int old_main()
     printf_float_array("RealIn", RealIn, MAXSIZE);
     printf_float_array("ImagIn", ImagIn, MAXSIZE);
 
-    /* regular*/
-    fft_float(MAXSIZE, invfft, RealIn, ImagIn, RealOut, ImagOut);
-
+    printf("Regular FFT\r\n");
+    printf("-----------\r\n");
+    fft_float(MAXSIZE, FALSE, RealIn, ImagIn, RealOut, ImagOut);
     printf_float_array("RealOut", RealOut, MAXSIZE);
     printf_float_array("ImagOut", ImagOut, MAXSIZE);
 
-    /* free(RealIn);
-     free(ImagIn);
-     free(RealOut);
-     free(ImagOut);
-     free(coeff);
-     free(amp);
-    */
+    printf("Inverse FFT\r\n");
+    printf("-----------\r\n");
+    fft_float(MAXSIZE, TRUE, RealIn, ImagIn, RealOut, ImagOut);
+    printf_float_array("RealOut", RealOut, MAXSIZE);
+    printf_float_array("ImagOut", ImagOut, MAXSIZE);
+
     return 0;
 }
