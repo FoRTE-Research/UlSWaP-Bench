471590b6739244575ccd9456c109208c32830e3f
Daniel Chiba <danchiba@vt.edu>
2024-04-11 19:00:38 -0400

    Cleanup limits
    Formatting and stdint types.

--- limits/limit_test.c
+++ limits/limit_test.c
@@ -1,86 +1,125 @@
-extern void hexstring (unsigned int d);
+#include <stdint.h>
 
-extern unsigned short ult ( unsigned short a, unsigned short b);
-extern unsigned short slt ( short a, short b);
+extern void hexstring(unsigned int d);
 
-extern unsigned short ulte ( unsigned short a, unsigned short b);
-extern unsigned short slte ( short a, short b);
+extern uint16_t ult(uint16_t a, uint16_t b);
+extern uint16_t slt(int16_t a, int16_t b);
 
-extern unsigned short ugt ( unsigned short a, unsigned short b);
-extern unsigned short sgt ( short a, short b);
+extern uint16_t ulte(uint16_t a, uint16_t b);
+extern uint16_t slte(int16_t a, int16_t b);
 
-extern unsigned short ugte ( unsigned short a, unsigned short b);
-extern unsigned short sgte ( short a, short b);
+extern uint16_t ugt(uint16_t a, uint16_t b);
+extern uint16_t sgt(int16_t a, int16_t b);
 
+extern uint16_t ugte(uint16_t a, uint16_t b);
+extern uint16_t sgte(int16_t a, int16_t b);
 
-unsigned short limit_test ( void )
+uint16_t limit_test(void)
 {
-    unsigned short ra;
-    short sa;
+    uint16_t ra;
+    int16_t sa;
 
-    for(ra=0;;ra++) if(ult(ra,7)==0) break;
+    for (ra = 0;; ra++)
+        if (ult(ra, 7) == 0)
+            break;
     hexstring(ra);
-    if(ra!=7) return(1);
+    if (ra != 7)
+        return (1);
 
-    for(ra=0xF000;;ra++) if(ult(ra,0xF007)==0) break;
+    for (ra = 0xF000;; ra++)
+        if (ult(ra, 0xF007) == 0)
+            break;
     hexstring(ra);
-    if(ra!=0xF007) return(1);
+    if (ra != 0xF007)
+        return (1);
 
-    for(sa=-7;;sa++) if(slt(sa,7)==0) break;
+    for (sa = -7;; sa++)
+        if (slt(sa, 7) == 0)
+            break;
     hexstring(sa);
-    if(sa!=7) return(1);
+    if (sa != 7)
+        return (1);
 
-    for(sa=-17;;sa++) if(slt(sa,-7)==0) break;
+    for (sa = -17;; sa++)
+        if (slt(sa, -7) == 0)
+            break;
     hexstring(sa);
-    if(sa!=-7) return(1);
+    if (sa != -7)
+        return (1);
 
-    for(ra=0;;ra++) if(ulte(ra,7)==0) break;
+    for (ra = 0;; ra++)
+        if (ulte(ra, 7) == 0)
+            break;
     hexstring(ra);
-    if(ra!=8) return(1);
+    if (ra != 8)
+        return (1);
 
-    for(ra=0xF000;;ra++) if(ulte(ra,0xF007)==0) break;
+    for (ra = 0xF000;; ra++)
+        if (ulte(ra, 0xF007) == 0)
+            break;
     hexstring(ra);
-    if(ra!=0xF008) return(1);
+    if (ra != 0xF008)
+        return (1);
 
-    for(ra=0xF000;;ra++) if(ulte(ra,7)==0) break;
+    for (ra = 0xF000;; ra++)
+        if (ulte(ra, 7) == 0)
+            break;
     hexstring(ra);
-    if(ra!=0xF000) return(1);
+    if (ra != 0xF000)
+        return (1);
 
-    for(sa=-7;;sa++) if(slte(sa,7)==0) break;
+    for (sa = -7;; sa++)
+        if (slte(sa, 7) == 0)
+            break;
     hexstring(sa);
-    if(sa!=8) return(1);
+    if (sa != 8)
+        return (1);
 
-    for(sa=-17;;sa++) if(slte(sa,-7)==0) break;
+    for (sa = -17;; sa++)
+        if (slte(sa, -7) == 0)
+            break;
     hexstring(sa);
-    if(sa!=-6) return(1);
+    if (sa != -6)
+        return (1);
 
-    for(ra=0;;ra++) if(ugt(ra,7)) break;
+    for (ra = 0;; ra++)
+        if (ugt(ra, 7))
+            break;
     hexstring(ra);
-    if(ra!=8) return(1);
+    if (ra != 8)
+        return (1);
 
-    for(ra=0xF000;;ra++) if(ugt(ra,0xF007)) break;
+    for (ra = 0xF000;; ra++)
+        if (ugt(ra, 0xF007))
+            break;
     hexstring(ra);
-    if(ra!=0xF008) return(1);
+    if (ra != 0xF008)
+        return (1);
 
-    for(ra=0;;ra++) if(ugte(ra,7)) break;
+    for (ra = 0;; ra++)
+        if (ugte(ra, 7))
+            break;
     hexstring(ra);
-    if(ra!=7) return(1);
+    if (ra != 7)
+        return (1);
 
-    for(ra=0xF000;;ra++) if(ugte(ra,0xF007)) break;
+    for (ra = 0xF000;; ra++)
+        if (ugte(ra, 0xF007))
+            break;
     hexstring(ra);
-    if(ra!=0xF007) return(1);
+    if (ra != 0xF007)
+        return (1);
 
-    return(0);
-
+    return (0);
 }
-unsigned short main ( void )
+
+int main(void)
 {
     hexstring(0x1234);
 
-    if(limit_test()) hexstring(0xBAD);
+    if (limit_test())
+        hexstring(0xBAD);
 
     hexstring(0x1234);
-    return(0);
+    return (0);
 }
-
-
--- limits/limit_test.stuff.c
+++ limits/limit_test.stuff.c
@@ -1,53 +1,61 @@
+#include <stdint.h>
 
-//if compiled together with the test code, the compiler optimizer
-//optimizes out the use of these funtions and simply puts the right
-//answer in the result without doing the work of the test
+// if compiled together with the test code, the compiler optimizer
+// optimizes out the use of these funtions and simply puts the right
+// answer in the result without doing the work of the test
 
-unsigned short ult ( unsigned short a, unsigned short b)
+uint16_t ult(uint16_t a, uint16_t b)
 {
-    if(a<b) return(1);
-    return(0);
+    if (a < b)
+        return (1);
+    return (0);
 }
 
-unsigned short slt ( short a, short b)
+uint16_t slt(int16_t a, int16_t b)
 {
-    if(a<b) return(1);
-    return(0);
+    if (a < b)
+        return (1);
+    return (0);
 }
 
-unsigned short ulte ( unsigned short a, unsigned short b)
+uint16_t ulte(uint16_t a, uint16_t b)
 {
-    if(a<=b) return(1);
-    return(0);
+    if (a <= b)
+        return (1);
+    return (0);
 }
 
-unsigned short slte ( short a, short b)
+uint16_t slte(int16_t a, int16_t b)
 {
-    if(a<=b) return(1);
-    return(0);
+    if (a <= b)
+        return (1);
+    return (0);
 }
 
-unsigned short ugt ( unsigned short a, unsigned short b)
+uint16_t ugt(uint16_t a, uint16_t b)
 {
-    if(a>b) return(1);
-    return(0);
+    if (a > b)
+        return (1);
+    return (0);
 }
 
-unsigned short sgt ( short a, short b)
+uint16_t sgt(int16_t a, int16_t b)
 {
-    if(a>b) return(1);
-    return(0);
+    if (a > b)
+        return (1);
+    return (0);
 }
 
-unsigned short ugte ( unsigned short a, unsigned short b)
+uint16_t ugte(uint16_t a, uint16_t b)
 {
-    if(a>=b) return(1);
-    return(0);
+    if (a >= b)
+        return (1);
+    return (0);
 }
 
-unsigned short sgte ( short a, short b)
+uint16_t sgte(int16_t a, int16_t b)
 {
-    if(a>=b) return(1);
-    return(0);
+    if (a >= b)
+        return (1);
+    return (0);
 }
-
