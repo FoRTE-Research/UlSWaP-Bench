22a1e47c38d468a890608557ad78b67df3144505
Daniel Chiba <danchiba@vt.edu>
2024-04-12 18:52:03 -0400

    Cleanup RC4
    Switched to stdint types.
    Formatting and minor refactoring.
    Removed redundant files.

--- rc4/rc4test.c
+++ rc4/rc4test.c
@@ -1,74 +1,61 @@
-
-//-------------------------------------------------------------------
-//-------------------------------------------------------------------
-
 #include "testdata.h"
-// #include "bareBench.h"
+#include <stdint.h>
 
-void hexstring ( unsigned int d ); //printf("%08X\n");
-void hexstrings ( unsigned int d ); //printf("%08X ");
+void hexstring(uint32_t d);
+void hexstrings(uint32_t d);
 
-void *memcpy(void *str1, const void *str2, unsigned int n)
-{
-    while(n > 0)
-    {
-        ((unsigned char *)str1)[n] = ((unsigned char *)str2)[n];
-        --n;
-    }
-    
-    return str1;
-}
-
-unsigned char edata[TESTDATALEN];
-unsigned char udata[TESTDATALEN];
+uint8_t ciphertext[TESTDATALEN];
+uint8_t plaintext[TESTDATALEN];
 
 /*        This code illustrates a sample implementation
-*                 of the Arcfour algorithm
-*         Copyright (c) April 29, 1997 Kalle Kaukonen.
-*                    All Rights Reserved.
-*
-* Redistribution and use in source and binary forms, with or
-* without modification, are permitted provided that this copyright
-* notice and disclaimer are retained.
-*
-* THIS SOFTWARE IS PROVIDED BY KALLE KAUKONEN AND CONTRIBUTORS ``AS
-* IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL KALLE
-* KAUKONEN OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-* OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-* THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
+ *                 of the Arcfour algorithm
+ *         Copyright (c) April 29, 1997 Kalle Kaukonen.
+ *                    All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that this copyright
+ * notice and disclaimer are retained.
+ *
+ * THIS SOFTWARE IS PROVIDED BY KALLE KAUKONEN AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL KALLE
+ * KAUKONEN OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 typedef struct
 {
-    unsigned int x;
-    unsigned int y;
-    unsigned char state[256];
+    uint32_t x;
+    uint32_t y;
+    uint8_t state[256];
 } ArcfourContext;
 
-void arcfour_init(ArcfourContext *ctx, const unsigned char *key, unsigned int key_len);
-unsigned int arcfour_byte(ArcfourContext *ctx);
-void arcfour_encrypt(ArcfourContext *ctx, unsigned char *dest, const unsigned char *src, unsigned int len);
+void arcfour_init(ArcfourContext *ctx, const uint8_t *key, uint32_t key_len);
+uint32_t arcfour_byte(ArcfourContext *ctx);
+void arcfour_encrypt(ArcfourContext *ctx, uint8_t *dest, const uint8_t *src, uint32_t len);
 
-void arcfour_init(ArcfourContext *ctx, const unsigned char *key, unsigned int key_len)
+void arcfour_init(ArcfourContext *ctx, const uint8_t *key, uint32_t key_len)
 {
-    unsigned int t, u;
-    unsigned int keyindex;
-    unsigned int stateindex;
-    unsigned char *state;
-    unsigned int counter;
+    uint32_t t, u;
+    uint32_t keyindex;
+    uint32_t stateindex;
+    uint8_t *state;
+    uint32_t counter;
 
     state = ctx->state;
     ctx->x = 0;
     ctx->y = 0;
-    for (counter = 0; counter < 256; counter++) state[counter] = counter;
+    for (counter = 0; counter < 256; counter++)
+    {
+        state[counter] = counter;
+    }
     keyindex = 0;
     stateindex = 0;
     for (counter = 0; counter < 256; counter++)
@@ -78,17 +65,19 @@ void arcfour_init(ArcfourContext *ctx, const unsigned char *key, unsigned int ke
         u = state[stateindex];
         state[stateindex] = t;
         state[counter] = u;
-        if (++keyindex >= key_len) keyindex = 0;
+        if (++keyindex >= key_len)
+        {
+            keyindex = 0;
+        }
     }
 }
 
-
-unsigned int arcfour_byte(ArcfourContext *ctx)
+uint32_t arcfour_byte(ArcfourContext *ctx)
 {
-    unsigned int x;
-    unsigned int y;
-    unsigned int sx, sy;
-    unsigned char *state;
+    uint32_t x;
+    uint32_t y;
+    uint32_t sx, sy;
+    uint8_t *state;
 
     state = ctx->state;
     x = (ctx->x + 1) & 0xff;
@@ -102,67 +91,60 @@ unsigned int arcfour_byte(ArcfourContext *ctx)
     return state[(sx + sy) & 0xff];
 }
 
-
-void arcfour_encrypt(ArcfourContext *ctx, unsigned char *dest, const unsigned char *src, unsigned int len)
+void arcfour_encrypt(ArcfourContext *ctx, uint8_t *dest, const uint8_t *src, uint32_t len)
 {
-    unsigned int i;
-    for (i = 0; i < len; i++) dest[i] = src[i] ^ arcfour_byte(ctx);
+    uint32_t i;
+    for (i = 0; i < len; i++)
+    {
+        dest[i] = src[i] ^ arcfour_byte(ctx);
+    }
 }
 
-
-
-
-
-int main ( void )
+int main(void)
 {
-    unsigned int errors;
-    int ra;
+    uint32_t errors = 0;
+    uint32_t ra;
 
-    unsigned char mykey[] =
-    {
+    uint8_t mykey[] = {
         0x29, 0x04, 0x19, 0x72,
         0xfb, 0x42, 0xba, 0x5f,
         0xc7, 0x12, 0x77, 0x12,
-        0xf1, 0x38, 0x29, 0xc9
-    };
+        0xf1, 0x38, 0x29, 0xc9};
 
     ArcfourContext mycontext;
 
     hexstring(0x12345678);
 
-    errors=0;
-
-    /* Initialize the algoritm */
+    /* Encryption */
     arcfour_init(&mycontext, mykey, 16);
+    arcfour_encrypt(&mycontext, ciphertext, testdata, TESTDATALEN);
 
-    /* Encrypt 13 bytes of the src string */
-
-    arcfour_encrypt(&mycontext, edata, testdata, TESTDATALEN);
-
-    /* Now "dest" contains the encrypted string. Do whatever
-       you please with it... */
-
+    /* Decryption */
     arcfour_init(&mycontext, mykey, 16);
+    arcfour_encrypt(&mycontext, plaintext, ciphertext, TESTDATALEN);
 
-    arcfour_encrypt(&mycontext, udata, edata, TESTDATALEN);
-
-    for(ra=0;ra<TESTDATALEN;ra++)
+    for (ra = 0; ra < TESTDATALEN; ra++)
     {
-        if(udata[ra]!=testdata[ra])
+        if (plaintext[ra] != testdata[ra])
         {
             errors++;
-            hexstrings(ra); hexstrings(udata[ra]); hexstring(testdata[ra]);
+            hexstrings(ra);
+            hexstrings(plaintext[ra]);
+            hexstring(testdata[ra]);
         }
-        if(errors>20) break;
+        if (errors > 20)
+        {
+            break;
+        }
     }
+
     hexstring(errors);
-    if(errors)
+    if (errors)
     {
         hexstring(0xBADBAD99);
-        return(1);
+        return (1);
     }
 
     hexstring(0x12345678);
-    return(0);
+    return (0);
 }
-
