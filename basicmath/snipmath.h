149d5bea82aec8d20a3d94d11e8cc47109a5ec79
Daniel Chiba <danchiba@vt.edu>
2024-04-12 11:52:13 -0400

    Cleanup basicmath
    Switched to stdint types.
    Removed redundant code.
    Fixed line endings for print statements.
    Fixed formatting.

--- basicmath/snipmath.h
+++ basicmath/snipmath.h
@@ -8,68 +8,35 @@
 #define SNIPMATH__H
 
 #include <math.h>
+#include <stdint.h>
 #include "sniptype.h"
-#include "round.h"
 
 /*
 **  Callable library functions begin here
 */
 
-void    SetBCDLen(int n);                             /* Bcdl.C         */
-long    BCDtoLong(char *BCDNum);                      /* Bcdl.C         */
-void    LongtoBCD(long num, char BCDNum[]);           /* Bcdl.C         */
-double  bcd_to_double(void *buf, size_t len,          /* Bcdd.C         */
-                      int digits);
-int     double_to_bcd(double arg, char *buf,          /* Bcdd.C         */
-                      size_t length, size_t digits );
-DWORD   ncomb1 (int n, int m);                        /* Combin.C       */
-DWORD   ncomb2 (int n, int m);                        /* Combin.C       */
-void    SolveCubic(double a, double b, double c,      /* Cubic.C        */
-                  double d, int *solutions,
-                  double *x);
-DWORD   dbl2ulong(double t);                          /* Dbl2Long.C     */
-long    dbl2long(double t);                           /* Dbl2Long.C     */
-double  dround(double x);                             /* Dblround.C     */
-
-/* Use #defines for Permutations and Combinations     -- Factoryl.C     */
-
-#define log10P(n,r) (log10factorial(n)-log10factorial((n)-(r)))
-#define log10C(n,r) (log10P((n),(r))-log10factorial(r))
-
-double  log10factorial(double N);                     /* Factoryl.C     */
-
-double  fibo(unsigned short term);                    /* Fibo.C         */
-double  frandom(int n);                               /* Frand.C        */
-double  ipow(double x, int n);                        /* Ipow.C         */
-int     ispow2(int x);                                /* Ispow2.C       */
-long    double ldfloor(long double a);                /* Ldfloor.C      */
-int     initlogscale(long dmax, long rmax);           /* Logscale.C     */
-long    logscale(long d);                             /* Logscale.C     */
-
-float   MSBINToIEEE(float f);                         /* Msb2Ieee.C     */
-float   IEEEToMSBIN(float f);                         /* Msb2Ieee.C     */
-int     perm_index (char pit[], int size);            /* Perm_Idx.C     */
-int     round_div(int n, int d);                      /* Rnd_Div.C      */
-long    round_ldiv(long n, long d);                   /* Rnd_Div.C      */
-double  rad2deg(double rad);                          /* Rad2Deg.C      */
-double  deg2rad(double deg);                          /* Rad2Deg.C      */
+void SolveCubic(double a, double b, double c, /* Cubic.C        */
+                double d, uint32_t *solutions,
+                double *x);
+double rad2deg(double rad); /* Rad2Deg.C      */
+double deg2rad(double deg); /* Rad2Deg.C      */
 
 #include "pi.h"
 #ifndef PHI
- #define PHI      ((1.0+sqrt(5.0))/2.0)         /* the golden number    */
- #define INV_PHI  (1.0/PHI)                     /* the golden ratio     */
+#define PHI ((1.0 + sqrt(5.0)) / 2.0) /* the golden number    */
+#define INV_PHI (1.0 / PHI)           /* the golden ratio     */
 #endif
 
 /*
 **  File: ISQRT.C
 */
 
-struct int_sqrt {
-      unsigned sqrt,
-               frac;
+struct int_sqrt
+{
+    uint32_t sqrt;
+    uint32_t frac;
 };
 
-void usqrt(unsigned long x, struct int_sqrt *q);
-
+void usqrt(uint32_t x, struct int_sqrt *q);
 
 #endif /* SNIPMATH__H */
