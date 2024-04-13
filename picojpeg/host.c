dc8f243eaff7004e0a9b480d4429fb420639561d
Daniel Chiba <danchiba@vt.edu>
2024-04-12 20:23:28 -0400

    Cleanup picojpeg
    Switch to stdint types.
    Removed unused files.
    Removed extraneous output.
    Fixed formatting for main files.

--- picojpeg/host.c
+++ picojpeg/host.c
@@ -2,145 +2,146 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #include "picojpeg.h"
 #include "g_jpegdata.h"
-unsigned int doff;
-//unsigned int dlen;
-//unsigned char odat[128][256][3];
+uint32_t doff;
+// uint32_t dlen;
+// uint8_t odat[128][256][3];
 
-unsigned char pjpeg_need_bytes_callback(unsigned char* pBuf, unsigned char buf_size, unsigned char *pBytes_actually_read, void *pCallback_data)
+uint8_t pjpeg_need_bytes_callback(uint8_t *pBuf, uint8_t buf_size, uint8_t *pBytes_actually_read, void *pCallback_data)
 {
-    unsigned int ra;
-//   uint n;
-  // pCallback_data;
+    uint32_t ra;
+    //   uint n;
+    // pCallback_data;
 
-    printf("callback buf_size %u\n",buf_size);
+    printf("callback buf_size %u\n", buf_size);
 
-//   n = min(g_nInFileSize - g_nInFileOfs, buf_size);
-   //if (n && (fread(pBuf, 1, n, g_pInFile) != n))
-      //return PJPG_STREAM_READ_ERROR;
-   //*pBytes_actually_read = (unsigned char)(n);
-   //g_nInFileOfs += n;
+    //   n = min(g_nInFileSize - g_nInFileOfs, buf_size);
+    // if (n && (fread(pBuf, 1, n, g_pInFile) != n))
+    // return PJPG_STREAM_READ_ERROR;
+    //*pBytes_actually_read = (uint8_t)(n);
+    // g_nInFileOfs += n;
 
-    //memcpy(pBuf,&data[doff],buf_size);
-    for(ra=0;ra<buf_size;ra++) pBuf[ra]=jpegdata[doff+ra];
-    doff+=buf_size;
+    // memcpy(pBuf,&data[doff],buf_size);
+    for (ra = 0; ra < buf_size; ra++)
+        pBuf[ra] = jpegdata[doff + ra];
+    doff += buf_size;
     *pBytes_actually_read = buf_size;
     return 0;
 }
 
-
-int main ( void )
+int32_t main(void)
 {
-    //FILE *fp;
-    //FILE *fpout;
-    unsigned char status;
+    // FILE *fp;
+    // FILE *fpout;
+    uint8_t status;
     pjpeg_image_info_t pInfo;
-    unsigned int sum;
-    //unsigned int ra;
-    unsigned int rb;
-    //unsigned int x,rx;
-    //unsigned int y,ry;
+    uint32_t sum;
+    // uint32_t ra;
+    uint32_t rb;
+    // uint32_t x,rx;
+    // uint32_t y,ry;
 
+    // fp=fopen("base.bmp","rb");
+    // if(fp==NULL) return(1);
+    // dlen=fread(odat,1,122,fp);
+    // fclose(fp);
+    // if(dlen!=122) return(1);
+    // fpout=fopen("out.bmp","wb");
+    // if(fpout==NULL) return(1);
+    // fwrite(odat,1,122,fpout);
 
-    //fp=fopen("base.bmp","rb");
-    //if(fp==NULL) return(1);
-    //dlen=fread(odat,1,122,fp);
-    //fclose(fp);
-    //if(dlen!=122) return(1);
-    //fpout=fopen("out.bmp","wb");
-    //if(fpout==NULL) return(1);
-    //fwrite(odat,1,122,fpout);
+    doff = 0;
 
-
-    doff=0;
-
-    status = pjpeg_decode_init(&pInfo, pjpeg_need_bytes_callback, NULL,0);
+    status = pjpeg_decode_init(&pInfo, pjpeg_need_bytes_callback, NULL, 0);
 
     if (status)
     {
         printf("pjpeg_decode_init() failed with status %u\n", status);
-        return(1);
+        return (1);
     }
-    //x=0;
-    //y=128-1;
-    sum=0;
-    while(status==0)
+    // x=0;
+    // y=128-1;
+    sum = 0;
+    while (status == 0)
     {
-        status=pjpeg_decode_mcu();
-        //printf("decode %u\n",ra);
-        if(status==0)
+        status = pjpeg_decode_mcu();
+        // printf("decode %u\n",ra);
+        if (status == 0)
         {
-            switch(pInfo.m_scanType)
+            switch (pInfo.m_scanType)
             {
-                case PJPG_GRAYSCALE: break;
-                case PJPG_YH1V1:
+            case PJPG_GRAYSCALE:
+                break;
+            case PJPG_YH1V1:
+            {
+                // rx=0;
+                // ry=0;
+                for (rb = 0; rb < 64; rb++)
                 {
-                    //rx=0;
-                    //ry=0;
-                    for(rb=0;rb<64;rb++)
-                    {
-                        sum+=pInfo.m_pMCUBufB[rb];
-                        sum+=pInfo.m_pMCUBufG[rb];
-                        sum+=pInfo.m_pMCUBufR[rb];
-                        //odat[y-ry][x+rx][0]=pInfo.m_pMCUBufB[rb];
-                        //odat[y-ry][x+rx][1]=pInfo.m_pMCUBufG[rb];
-                        //odat[y-ry][x+rx][2]=pInfo.m_pMCUBufR[rb];
-                        //rx++;
-                        //if(rx>=8)
-                        //{
-                            //rx=0;
-                            //ry++;
-                        //}
-                    }
-                    //x+=8;
-                    //if(x>=256)
+                    sum += pInfo.m_pMCUBufB[rb];
+                    sum += pInfo.m_pMCUBufG[rb];
+                    sum += pInfo.m_pMCUBufR[rb];
+                    // odat[y-ry][x+rx][0]=pInfo.m_pMCUBufB[rb];
+                    // odat[y-ry][x+rx][1]=pInfo.m_pMCUBufG[rb];
+                    // odat[y-ry][x+rx][2]=pInfo.m_pMCUBufR[rb];
+                    // rx++;
+                    // if(rx>=8)
                     //{
-                        //y-=8;
-                        //x=0;
+                    // rx=0;
+                    // ry++;
                     //}
-                    //// PJPG_H1V1: Each MCU contains is decoded to a single block of 8x8 RGB pixels.
-////   unsigned char *m_pMCUBufR;
-                    //for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufR[rb];
-                    //fwrite(pInfo.m_pMCUBufR,1,64,fpout);
-                    //fwrite(pInfo.m_pMCUBufR,1,64,fp);
-   ////unsigned char *m_pMCUBufG;
-                    //for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufG[rb];
-                    //fwrite(pInfo.m_pMCUBufG,1,64,fpout);
-                    //fwrite(pInfo.m_pMCUBufG,1,64,fp);
-   ////unsigned char *m_pMCUBufB;
-                    //for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufB[rb];
-                    //fwrite(pInfo.m_pMCUBufB,1,64,fpout);
-                    //fwrite(pInfo.m_pMCUBufB,1,64,fp);
-                    break;
                 }
-                case PJPG_YH2V1: break;
-                case PJPG_YH1V2: break;
-                case PJPG_YH2V2: break;
+                // x+=8;
+                // if(x>=256)
+                //{
+                // y-=8;
+                // x=0;
+                //}
+                //// PJPG_H1V1: Each MCU contains is decoded to a single block of 8x8 RGB pixels.
+                ////   uint8_t *m_pMCUBufR;
+                // for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufR[rb];
+                // fwrite(pInfo.m_pMCUBufR,1,64,fpout);
+                // fwrite(pInfo.m_pMCUBufR,1,64,fp);
+                ////uint8_t *m_pMCUBufG;
+                // for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufG[rb];
+                // fwrite(pInfo.m_pMCUBufG,1,64,fpout);
+                // fwrite(pInfo.m_pMCUBufG,1,64,fp);
+                ////uint8_t *m_pMCUBufB;
+                // for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufB[rb];
+                // fwrite(pInfo.m_pMCUBufB,1,64,fpout);
+                // fwrite(pInfo.m_pMCUBufB,1,64,fp);
+                break;
+            }
+            case PJPG_YH2V1:
+                break;
+            case PJPG_YH1V2:
+                break;
+            case PJPG_YH2V2:
+                break;
             }
         }
     }
-    if(status!=PJPG_NO_MORE_BLOCKS)
+    if (status != PJPG_NO_MORE_BLOCKS)
     {
-        printf("status %u\n",status);
-        return(1);
+        printf("status %u\n", status);
+        return (1);
     }
     printf("----\n");
-    printf("%d\n",pInfo.m_width);
-    printf("%d\n",pInfo.m_height);
-    printf("%d\n",pInfo.m_comps);
-    printf("%d\n",pInfo.m_MCUSPerRow);
-    printf("%d\n",pInfo.m_MCUSPerCol);
-    printf("%d\n",pInfo.m_scanType);
-    printf("%d\n",pInfo.m_MCUWidth);
-    printf("%d\n",pInfo.m_MCUHeight);
-    printf("---- sum 0x%08X %u\n",sum,sum);
+    printf("%d\n", pInfo.m_width);
+    printf("%d\n", pInfo.m_height);
+    printf("%d\n", pInfo.m_comps);
+    printf("%d\n", pInfo.m_MCUSPerRow);
+    printf("%d\n", pInfo.m_MCUSPerCol);
+    printf("%d\n", pInfo.m_scanType);
+    printf("%d\n", pInfo.m_MCUWidth);
+    printf("%d\n", pInfo.m_MCUHeight);
+    printf("---- sum 0x%08X %u\n", sum, sum);
 
+    // fwrite(odat,1,sizeof(odat),fpout);
+    // fclose(fpout);
 
-    //fwrite(odat,1,sizeof(odat),fpout);
-    //fclose(fpout);
-
-    return(0);
+    return (0);
 }
-
--- picojpeg/picojpegtest.c
+++ picojpeg/picojpegtest.c
@@ -1,95 +1,94 @@
-
-void hexstring ( unsigned int d ); //printf("%08X\n");
-void hexstrings ( unsigned int d ); //printf("%08X ");
-
 #include <stdio.h>
+#include <stdint.h>
 #include "picojpeg.h"
 #include "g_jpegdata.h"
 
-unsigned int doff;
+void hexstring(uint32_t d);
 
-unsigned char pjpeg_need_bytes_callback(unsigned char* pBuf, unsigned char buf_size, unsigned char *pBytes_actually_read, void *pCallback_data)
+uint32_t doff;
+
+uint8_t pjpeg_need_bytes_callback(uint8_t *pBuf, uint8_t buf_size, uint8_t *pBytes_actually_read, void *pCallback_data)
 {
-    unsigned int ra;
-//   uint n;
-  // pCallback_data;
+    uint32_t ra;
+    //   uint n;
+    // pCallback_data;
 
-//   n = min(g_nInFileSize - g_nInFileOfs, buf_size);
-   //if (n && (fread(pBuf, 1, n, g_pInFile) != n))
-      //return PJPG_STREAM_READ_ERROR;
-   //*pBytes_actually_read = (unsigned char)(n);
-   //g_nInFileOfs += n;
+    //   n = min(g_nInFileSize - g_nInFileOfs, buf_size);
+    // if (n && (fread(pBuf, 1, n, g_pInFile) != n))
+    // return PJPG_STREAM_READ_ERROR;
+    //*pBytes_actually_read = (uint8_t)(n);
+    // g_nInFileOfs += n;
 
-    //memcpy(pBuf,&jpegdata[doff],buf_size);
-    for(ra=0;ra<buf_size;ra++) pBuf[ra]=jpegdata[doff+ra];
-    doff+=buf_size;
+    // memcpy(pBuf,&jpegdata[doff],buf_size);
+    for (ra = 0; ra < buf_size; ra++)
+        pBuf[ra] = jpegdata[doff + ra];
+    doff += buf_size;
     *pBytes_actually_read = buf_size;
     return 0;
 }
 
-
-int main ( void )
+int32_t main(void)
 {
-    unsigned char status;
+    uint8_t status;
     pjpeg_image_info_t pInfo;
-    unsigned int sum;
-    unsigned int rb;
+    uint32_t sum;
+    uint32_t rb;
 
+    doff = 0;
 
-
-    doff=0;
-
-    status = pjpeg_decode_init(&pInfo, pjpeg_need_bytes_callback,(void *)0,0);
+    status = pjpeg_decode_init(&pInfo, pjpeg_need_bytes_callback, (void *)0, 0);
 
     if (status)
     {
-        return(1);
+        return (1);
     }
-    sum=0;
-    while(status==0)
+    sum = 0;
+    while (status == 0)
     {
-        status=pjpeg_decode_mcu();
-        //printf("decode %u\n",ra);
-        if(status==0)
+        status = pjpeg_decode_mcu();
+        // printf("decode %u\n",ra);
+        if (status == 0)
         {
-            switch(pInfo.m_scanType)
+            switch (pInfo.m_scanType)
             {
-                case PJPG_GRAYSCALE: break;
-                case PJPG_YH1V1:
+            case PJPG_GRAYSCALE:
+                break;
+            case PJPG_YH1V1:
+            {
+                for (rb = 0; rb < 64; rb++)
                 {
-                    for(rb=0;rb<64;rb++)
-                    {
-                        sum+=pInfo.m_pMCUBufB[rb];
-                        sum+=pInfo.m_pMCUBufG[rb];
-                        sum+=pInfo.m_pMCUBufR[rb];
-                    }
-                    //// PJPG_H1V1: Each MCU contains is decoded to a single block of 8x8 RGB pixels.
-////   unsigned char *m_pMCUBufR;
-                    //for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufR[rb];
-                    //fwrite(pInfo.m_pMCUBufR,1,64,fpout);
-                    //fwrite(pInfo.m_pMCUBufR,1,64,fp);
-   ////unsigned char *m_pMCUBufG;
-                    //for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufG[rb];
-                    //fwrite(pInfo.m_pMCUBufG,1,64,fpout);
-                    //fwrite(pInfo.m_pMCUBufG,1,64,fp);
-   ////unsigned char *m_pMCUBufB;
-                    //for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufB[rb];
-                    //fwrite(pInfo.m_pMCUBufB,1,64,fpout);
-                    //fwrite(pInfo.m_pMCUBufB,1,64,fp);
-                    break;
+                    sum += pInfo.m_pMCUBufB[rb];
+                    sum += pInfo.m_pMCUBufG[rb];
+                    sum += pInfo.m_pMCUBufR[rb];
                 }
-                case PJPG_YH2V1: break;
-                case PJPG_YH1V2: break;
-                case PJPG_YH2V2: break;
+                //// PJPG_H1V1: Each MCU contains is decoded to a single block of 8x8 RGB pixels.
+                ////   uint8_t *m_pMCUBufR;
+                // for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufR[rb];
+                // fwrite(pInfo.m_pMCUBufR,1,64,fpout);
+                // fwrite(pInfo.m_pMCUBufR,1,64,fp);
+                ////uint8_t *m_pMCUBufG;
+                // for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufG[rb];
+                // fwrite(pInfo.m_pMCUBufG,1,64,fpout);
+                // fwrite(pInfo.m_pMCUBufG,1,64,fp);
+                ////uint8_t *m_pMCUBufB;
+                // for(rb=0;rb<64;rb++) sum+=pInfo.m_pMCUBufB[rb];
+                // fwrite(pInfo.m_pMCUBufB,1,64,fpout);
+                // fwrite(pInfo.m_pMCUBufB,1,64,fp);
+                break;
+            }
+            case PJPG_YH2V1:
+                break;
+            case PJPG_YH1V2:
+                break;
+            case PJPG_YH2V2:
+                break;
             }
         }
     }
-    if(status!=PJPG_NO_MORE_BLOCKS)
+    if (status != PJPG_NO_MORE_BLOCKS)
     {
-        return(1);
+        return (1);
     }
-    printf("---- sum 0x%08X 0x%08X\n",sum,sum);
     hexstring(sum);
-    return(0);
+    return (0);
 }
-
