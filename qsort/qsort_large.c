46b90398e031342a66accd95e86f2be6e93b6953
Daniel Chiba <danchiba@vt.edu>
2024-04-15 15:48:12 -0400

    Cleanup qsort
    Formatting and stdint types.

--- qsort/qsort_large.c
+++ qsort/qsort_large.c
@@ -6,37 +6,34 @@
 
 #define UNLIMIT
 
-
-/*struct my3DVertexStruct {
-  int x, y, z;
-  double distance;
-};*/
-
-int compare(const void *elem1, const void *elem2)
+int32_t compare(const void *elem1, const void *elem2)
 {
-  /* D = [(x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2]^(1/2) */
-  /* sort based on distances from the origin... */
+    /* D = [(x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2]^(1/2) */
+    /* sort based on distances from the origin... */
 
-  double distance1, distance2;
+    double distance1, distance2;
 
-  distance1 = (*((struct my3DVertexStruct *)elem1)).distance;
-  distance2 = (*((struct my3DVertexStruct *)elem2)).distance;
+    distance1 = (*((struct my3DVertexStruct *)elem1)).distance;
+    distance2 = (*((struct my3DVertexStruct *)elem2)).distance;
 
-  return (distance1 > distance2) ? 1 : ((distance1 == distance2) ? 0 : -1);
+    return (distance1 > distance2) ? 1 : ((distance1 == distance2) ? 0 : -1);
 }
 
+int main(void)
+{
+    uint32_t i, count = 0;
 
-int
-main(void) {
-  int i,count=0;
+    for (count = 0; count < sizeof(array) / sizeof(struct my3DVertexStruct); ++count)
+    {
+        array[count].distance = sqrt(pow(array[count].x, 2) + pow(array[count].y, 2) + pow(array[count].z, 2));
+    }
 
-  for(count = 0; count < sizeof(array)/sizeof(struct my3DVertexStruct); ++count)
-	 array[count].distance = sqrt(pow(array[count].x, 2) + pow(array[count].y, 2) + pow(array[count].z, 2));
-  
-  printf("\nSorting %d vectors based on distance from the origin.\n\n",count);
-  qsort(array,count,sizeof(struct my3DVertexStruct),compare);
-  
-  for(i=0;i<count;i++)
-    printf("%d %d %d\n", array[i].x, array[i].y, array[i].z);
-  return 0;
+    printf("Sorting %u vectors based on distance from the origin.\r\n\r\n", count);
+    qsort(array, count, sizeof(struct my3DVertexStruct), compare);
+
+    for (i = 0; i < count; i++)
+    {
+        printf("%d %d %d\r\n", array[i].x, array[i].y, array[i].z);
+    }
+    return 0;
 }
