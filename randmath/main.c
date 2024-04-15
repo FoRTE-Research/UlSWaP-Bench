1c8733d98cf32cc559ce0b56e07ffd638968a212
Daniel Chiba <danchiba@vt.edu>
2024-04-15 16:03:08 -0400

    Cleanup randmath
    Formatting, stdint types and filename change.

---
+++ randmath/main.c
@@ -0,0 +1,8 @@
+unsigned int abcmath(void);
+void hexstring(unsigned int d);
+
+int main(void)
+{
+    hexstring(abcmath());
+    return (0);
+}
--- randmath/randmath.c
+++ randmath/randmath.c
@@ -3,96 +3,113 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdint.h>
 
 FILE *fpout;
 
 //------------------------------------------------------------------------
-unsigned int prand32 ( unsigned int x )
+uint32_t prand32(uint32_t x)
 {
-    if(x&1)
+    if (x & 1)
     {
-        x=x>>1;
-        x=x^0xBF9EC099;
+        x = x >> 1;
+        x = x ^ 0xBF9EC099;
     }
     else
     {
-        x=x>>1;
+        x = x >> 1;
     }
-    return(x);
+    return (x);
 }
 
-
-int main ( void )
+int main(void)
 {
-    unsigned int prand;
-    unsigned int ra;
-    unsigned int rb;
-    unsigned int rc;
-    unsigned int rd;
+    uint32_t prand;
+    uint32_t ra;
+    uint32_t rb;
+    uint32_t rc;
+    uint32_t rd;
 
-    prand=0x1234;
+    prand = 0x1234;
 
-    fpout=fopen("g_abcmath.c","wt");
-    if(fpout==NULL) return(1);
-
-    fprintf(fpout,"\n");
-    fprintf(fpout,"unsigned int prand32 ( unsigned int x )\n");
-    fprintf(fpout,"{\n");
-    fprintf(fpout,"    if(x&1)\n");
-    fprintf(fpout,"    {\n");
-    fprintf(fpout,"        x=x>>1;\n");
-    fprintf(fpout,"        x=x^0xBF9EC099;\n");
-    fprintf(fpout,"    }\n");
-    fprintf(fpout,"    else\n");
-    fprintf(fpout,"    {\n");
-    fprintf(fpout,"        x=x>>1;\n");
-    fprintf(fpout,"    }\n");
-    fprintf(fpout,"    return(x);\n");
-    fprintf(fpout,"}\n");
-    fprintf(fpout,"\n");
-    fprintf(fpout,"unsigned int prand;\n");
-    for(ra=0;ra<8;ra++) fprintf(fpout,"unsigned int %c;\n",'a'+ra);
-    fprintf(fpout,"\n");
-    fprintf(fpout,"unsigned int abcmath ( void )\n");
-    fprintf(fpout,"{\n");
-    for(;ra<16;ra++) fprintf(fpout,"unsigned int %c;\n",'a'+ra);
-    fprintf(fpout,"\n");
-    fprintf(fpout,"prand=0xABC;\n");
-    for(ra=0;ra<16;ra++)
+    fpout = fopen("g_abcmath.c", "wt");
+    if (fpout == NULL)
     {
-        fprintf(fpout,"prand=prand32(prand); %c=prand;\n",'a'+ra);
+        return (1);
     }
-    for(ra=0;ra<2000;ra++)
+
+    fprintf(fpout, "#include <stdint.h>\n\n");
+    fprintf(fpout, "uint32_t prand32 ( uint32_t x )\n");
+    fprintf(fpout, "{\n");
+    fprintf(fpout, "    if(x&1)\n");
+    fprintf(fpout, "    {\n");
+    fprintf(fpout, "        x=x>>1;\n");
+    fprintf(fpout, "        x=x^0xBF9EC099;\n");
+    fprintf(fpout, "    }\n");
+    fprintf(fpout, "    else\n");
+    fprintf(fpout, "    {\n");
+    fprintf(fpout, "        x=x>>1;\n");
+    fprintf(fpout, "    }\n");
+    fprintf(fpout, "    return(x);\n");
+    fprintf(fpout, "}\n");
+    fprintf(fpout, "\n");
+    fprintf(fpout, "uint32_t prand;\n");
+    for (ra = 0; ra < 8; ra++)
     {
-        prand=prand32(prand);
-        prand=prand32(prand);
-        prand=prand32(prand);
-        rb=(prand>> 0)&15;
-        while(1)
+        fprintf(fpout, "uint32_t %c;\n", 'a' + ra);
+    }
+    fprintf(fpout, "\n");
+    fprintf(fpout, "uint32_t abcmath ( void )\n");
+    fprintf(fpout, "{\n");
+    for (; ra < 16; ra++)
+    {
+        fprintf(fpout, "uint32_t %c;\n", 'a' + ra);
+    }
+    fprintf(fpout, "\n");
+    fprintf(fpout, "prand=0xABC;\n");
+    for (ra = 0; ra < 16; ra++)
+    {
+        fprintf(fpout, "prand=prand32(prand); %c=prand;\n", 'a' + ra);
+    }
+    for (ra = 0; ra < 2000; ra++)
+    {
+        prand = prand32(prand);
+        prand = prand32(prand);
+        prand = prand32(prand);
+        rb = (prand >> 0) & 15;
+        while (1)
         {
-            prand=prand32(prand);
-            prand=prand32(prand);
-            prand=prand32(prand);
-            rc=(prand>> 8)&15;
-            if(rb==rc) continue;
+            prand = prand32(prand);
+            prand = prand32(prand);
+            prand = prand32(prand);
+            rc = (prand >> 8) & 15;
+            if (rb == rc)
+            {
+                continue;
+            }
             break;
         }
-        while(1)
+        while (1)
         {
-            prand=prand32(prand);
-            prand=prand32(prand);
-            prand=prand32(prand);
-            prand=prand32(prand);
-            rd=(prand>>16)&15;
-            if(rb==rc) continue;
-            if(rb==rd) continue;
+            prand = prand32(prand);
+            prand = prand32(prand);
+            prand = prand32(prand);
+            prand = prand32(prand);
+            rd = (prand >> 16) & 15;
+            if (rb == rc)
+            {
+                continue;
+            }
+            if (rb == rd)
+            {
+                continue;
+            }
             break;
         }
-        prand=prand32(prand);
-        prand=prand32(prand);
-        prand=prand32(prand);
-        switch((prand>>24)&15)
+        prand = prand32(prand);
+        prand = prand32(prand);
+        prand = prand32(prand);
+        switch ((prand >> 24) & 15)
         {
             case  0: fprintf(fpout,"%c = %c + %c;\n",rb+'a',rc+'a',rd+'a'); break;
             case  1: fprintf(fpout,"%c = %c - %c;\n",rb+'a',rc+'a',rd+'a'); break;
@@ -102,26 +119,24 @@ int main ( void )
             case  5: fprintf(fpout,"%c = %c | %c;\n",rb+'a',rc+'a',rd+'a'); break;
             case  6: fprintf(fpout,"%c = %c + %c;\n",rb+'a',rc+'a',rd+'a'); break;
             case  7: fprintf(fpout,"prand=prand32(prand); %c = prand;\n",rb+'a'); break;
-
             case  8: fprintf(fpout,"if(%c>%c) { prand=prand32(prand); %c = prand; }\n",rb+'a',rc+'a',rd+'a'); break;
             case  9: fprintf(fpout,"if(%c<%c) { prand=prand32(prand); %c = prand; }\n",rb+'a',rc+'a',rd+'a'); break;
             case 10: fprintf(fpout,"if(%c==%c) { prand=prand32(prand); %c = prand; }\n",rb+'a',rc+'a',rd+'a'); break;
             case 11: fprintf(fpout,"if(%c>=%c) { prand=prand32(prand); %c = prand; }\n",rb+'a',rc+'a',rd+'a'); break;
             case 12: fprintf(fpout,"if(%c<=%c) { prand=prand32(prand); %c = prand; }\n",rb+'a',rc+'a',rd+'a'); break;
-            default:
+        default:
                 fprintf(fpout,"%c = %c | %c;\n",rb+'a',rc+'a',rd+'a'); break;
-                break;
+            break;
         }
     }
 
-    for(ra=1;ra<16;ra++)
+    for (ra = 1; ra < 16; ra++)
     {
-        fprintf(fpout,"a += %c;\n",'a'+ra);
+        fprintf(fpout, "a += %c;\n", 'a' + ra);
     }
-    fprintf(fpout,"return(a);\n");
+    fprintf(fpout, "return(a);\n");
 
-
-    fprintf(fpout,"}\n");
-    fprintf(fpout,"\n");
-    return(0);
+    fprintf(fpout, "}\n");
+    fprintf(fpout, "\n");
+    return (0);
 }
