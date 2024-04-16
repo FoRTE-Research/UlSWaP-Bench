d66521b93ce58cbe5c1b7e8b51e3fd104944cdd2
Daniel Chiba <danchiba@vt.edu>
2024-04-16 16:07:47 -0400

    New directory structure
    Refactored directory hierarchy to separate benchmark source
    code from architecture specific source code and build files.

--- Makefile.mk
+++ Makefile.mk
@@ -2,5 +2,5 @@ ifeq (,$(findstring $(ARCH), riscv arm native spike))
 $(error Usage: make ARCH={riscv|arm|native|spike})
 endif
 
-ARCH_DIR=../$(ARCH)
+ARCH_DIR=../../arch/$(ARCH)
 include $(ARCH_DIR)/Makefile.mk
