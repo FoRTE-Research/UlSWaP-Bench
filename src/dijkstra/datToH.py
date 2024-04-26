bbf1f86c325bc0fc4cd6399371bade3686823e17
Daniel Chiba <danchiba@vt.edu>
2024-04-25 20:59:51 -0400

    Update Dijkstra
    Created a new script to generate input for dijkstra and updated
    input.h.

--- src/dijkstra/datToH.py
+++ src/dijkstra/datToH.py
@@ -3,28 +3,24 @@ import sys
 
 # Need to know how many rows, since C requires that to be specified
 numRows = 0
-infile = open("input.dat")
+infile = open('input.dat')
 for line in infile:
     if len(line.strip()) > 0:
         numRows += 1
 infile.close()
 
-print "unsigned int AdjMatrix[",
-print numRows,
-print "][",
-print numRows,
-print "] = {"
+print(f'#define NUM_NODES {numRows}')
+print(f'unsigned int AdjMatrix[{numRows}][{numRows}] = ')
+print('{')
 
-infile = open("input.dat")
+infile = open('input.dat')
 
 for line in infile:
     parts = line.strip().split()
-    print "{",
+    print('{', end='')
     for part in parts:
-        print int(part),
-        print ",",
-    print "},"
+        # print with padding using spaces
+        print('{:2d}, '.format(int(part)), end='')
+    print('},')
 
-print "};"
-
-    
+print('};')
