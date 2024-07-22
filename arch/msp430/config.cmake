414526262a0fa47116b1f242bf86a492ea1ba1a1
Harrison Williams <hrwill@vt.edu>
2024-07-22 14:04:55 -0400

    adpcm encode/decode working on msp430

---
+++ arch/msp430/config.cmake
@@ -0,0 +1,25 @@
+function (set_msp430_config)
+    message(STATUS "Setting MSP430 configuration")
+
+    set(PICOLIBC_ROOT $ENV{HOME}/picolibc/build-msp430)
+    set(MSP430_ROOT $ENV{HOME}/ti/msp430-gcc)
+
+    set(LIB_PATH ${PICOLIBC_ROOT}/build-msp430)
+    set(CC_PATH ${MSP430_ROOT}/bin)
+
+    set(CMAKE_C_COMPILER ${CC_PATH}/msp430-elf-gcc PARENT_SCOPE)
+    set(CMAKE_ASM_COMPILER ${CC_PATH}/msp430-elf-gcc PARENT_SCOPE)
+
+    set(GENERAL_FLAGS "-Wall;-fno-builtin;-ffreestanding;-fno-optimize-sibling-calls;-fno-builtin-fma;-ffp-contract=off;-ffunction-sections")
+    #set(GENERAL_FLAGS "-Wall")
+    set(MSP430_FLAGS "-mlarge;-mmcu=msp430fr5994;-specs=${PICOLIBC_ROOT}/picolibc.specs;-T${ARCH_DIR}/memmap.ld;-DCUSTOM_ARCH_STARTUP")
+
+    set(ARCH_OBJDUMP "${CC_PATH}/msp430-elf-objdump" PARENT_SCOPE)
+
+#    set(ARCH_LINK_DIRS "${MSP430_ROOT}/include" PARENT_SCOPE)
+    set(ARCH_INC_DIRS "${MSP430_ROOT}/include" PARENT_SCOPE)
+
+    set(ARCH_FLAGS "${GENERAL_FLAGS};${MSP430_FLAGS}" PARENT_SCOPE)
+    set(ARCH_LINK_FLAGS "${GENERAL_FLAGS};${MSP430_FLAGS}" PARENT_SCOPE)
+    set(ARCH_SOURCES "${ARCH_DIR}/supportFuncs.c;${ARCH_DIR}/vectors.S" PARENT_SCOPE)
+endfunction()
