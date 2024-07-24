925ac98268bdf1c353f5cb901adb87f0445f0ac2
Daniel Chiba <danchiba@vt.edu>
2024-07-24 19:01:59 -0400

    Add jpeg_encode
    Added a new benchmark to encode a raw image into JPEG.
    Renamed picojpeg to jpeg_decode to disambiguate.

---
+++ src/jpeg_encode/jpeg_encode_test.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "jpec.h"
+#include "input.h"
+
+#define OUTPUT_IMAGE_NAME "output.jpg"
+#define OUTPUT_IMAGE_QUALITY 90
+
+#define INPUT_IMAGE_WIDTH 176
+#define INPUT_IMAGE_HEIGHT 104
+
+int32_t benchmark_main()
+{
+    uint16_t w = INPUT_IMAGE_WIDTH;
+    uint16_t h = INPUT_IMAGE_HEIGHT;
+    jpec_enc_t *e = jpec_enc_new2(test_data, w, h, OUTPUT_IMAGE_QUALITY);
+
+    printf("Image width: %u\r\n", w);
+    printf("Image height: %u\r\n", h);
+    printf("Output image quality: %u\r\n", OUTPUT_IMAGE_QUALITY);
+
+    int32_t len;
+    const uint8_t *jpeg = jpec_enc_run(e, &len);
+
+    uint32_t checksum = 0;
+    for (int32_t i = 0; i < len; i++)
+    {
+        checksum += jpeg[i];
+    }
+    printf("Output image size: %d\r\n", len);
+    printf("Output image checksum: %u\r\n", checksum);
+
+#if HOST_TEST
+    FILE *file = fopen(OUTPUT_IMAGE_NAME, "wb");
+    fwrite(jpeg, sizeof(uint8_t), len, file);
+    fclose(file);
+    printf("Output image written to %s\r\n", OUTPUT_IMAGE_NAME);
+#endif  // HOST_TEST
+
+    return 0;
+}
