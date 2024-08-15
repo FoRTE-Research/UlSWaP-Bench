9bc1f01bef71dbd503664120fa963438ffeecb89
Harrison Williams <hrwill@vt.edu>
2024-08-15 10:05:06 -0400

    Sensor fusion benchmark

---
+++ src/sensor_fusion/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "c6h6_model.h" // emlearn generated model
+#include "data.h"
+
+int benchmark_main(void){
+  float out;
+  int i, j;
+  for(i = TESTCASES-1; i >= 0; i--){
+    out = c6h6_model_predict(testFeatures[i], 7);
+    for(j = 0; j < 6; j++){
+      printf("%hu, ", testFeatures[i][j]);
+    }
+    printf("%hu: ", testFeatures[i][6]);
+    printf("%f\r\n", out);
+  }
+  return 0;
+}
