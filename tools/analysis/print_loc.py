fe2924812236cc5a7c546604dbe12d9d5adec735
Daniel Chiba <danchiba@vt.edu>
2024-09-09 18:52:44 -0400

    Separate LOC count
    Report separate line count numbers for .c and .h files,
    excluding input.h.

--- tools/analysis/print_loc.py
+++ tools/analysis/print_loc.py
@@ -5,7 +5,7 @@ import sys
 BENCHMARK_TOP_DIR = f'{os.path.dirname(os.path.abspath(__file__))}/../..'
 BENCHMARK_SRC_DIR = f'{BENCHMARK_TOP_DIR}/src'
 
-def get_benchmarks_loc() -> dict[str, int]:
+def get_benchmarks_loc() -> dict[str, (int, int)]:
     # Loop over all directories in BENCHMARK_SRC_DIR
     loc = {}
     for benchmark in os.listdir(BENCHMARK_SRC_DIR):
@@ -13,7 +13,7 @@ def get_benchmarks_loc() -> dict[str, int]:
         if not os.path.isdir(benchmark_path):
             continue
 
-        cmd = f'cloc {benchmark_path} --force-lang=C,h --include-lang=C'
+        cmd = f'cloc {benchmark_path} --include-lang="C,C/C++ Header" -not-match-f=input.h --hide-rate --quiet'
 
         proc = sp.run(cmd, shell=True, capture_output=True)
         if proc.returncode != 0:
@@ -23,8 +23,9 @@ def get_benchmarks_loc() -> dict[str, int]:
 
         output = proc.stdout.decode()
         cloc_lines = output.splitlines()
-        total_loc = cloc_lines[-2].split()[-1]
-        loc[benchmark] = int(total_loc)
+        c_loc = cloc_lines[5].split()[-1]
+        h_loc = cloc_lines[6].split()[-1] if 'Header' in cloc_lines[6] else 0
+        loc[benchmark] = (int(c_loc), int(h_loc))
 
     # Sort the dictionary by benchmark name
     loc = dict(sorted(loc.items()))
@@ -35,7 +36,7 @@ def get_benchmarks_loc() -> dict[str, int]:
 def main():
     loc = get_benchmarks_loc()
     for benchmark, loc in loc.items():
-        print(f'{benchmark},{loc}')
+        print(f'{benchmark},{loc[0]},{loc[1]}')
 
 
 if __name__ == '__main__':
