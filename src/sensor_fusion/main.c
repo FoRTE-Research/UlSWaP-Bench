bf93dff5269a80254e6515d025dc225f3492ce8c
Daniel Chiba <danchiba@vt.edu>
2024-09-13 16:25:53 -0400

    sensor_fusion fix
    Fix printing of sensor_fusion output in RISC-V and make
    output volatile.

--- src/sensor_fusion/main.c
+++ src/sensor_fusion/main.c
@@ -4,16 +4,22 @@
 #include "c6h6_model.h" // emlearn generated model
 #include "data.h"
 
-int benchmark_main(void){
-  float out;
-  int i, j;
-  for(i = TESTCASES-1; i >= 0; i--){
-    out = c6h6_model_predict(testFeatures[i], 7);
-    for(j = 0; j < 6; j++){
-      printf("%hu, ", testFeatures[i][j]);
+int benchmark_main(void)
+{
+    volatile float out;
+    int i, j;
+    for (i = TESTCASES - 1; i >= 0; i--)
+    {
+        out = c6h6_model_predict(testFeatures[i], 7);
+        for (j = 0; j < 6; j++)
+        {
+            printf("%hu, ", testFeatures[i][j]);
+        }
+        printf("%hu: ", testFeatures[i][6]);
+        printf("%f\r\n", printf_float(out));
     }
-    printf("%hu: ", testFeatures[i][6]);
-    printf("%f\r\n", out);
-  }
-  return 0;
+
+    (void)out;
+
+    return 0;
 }
