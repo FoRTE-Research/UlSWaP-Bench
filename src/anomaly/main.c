21de21d3edb5acb4a1a8ff7d9e005db78e5afa42
Daniel Chiba <danchiba@vt.edu>
2024-09-16 14:47:18 -0400

    Anomaly fix
    Fix printing of anomaly output and make output volatile.

--- src/anomaly/main.c
+++ src/anomaly/main.c
@@ -5,68 +5,85 @@
 
 #include "svm.h"
 #include "data.h"
+#include "common.h"
 
 // RMS and crest factor (peak/RMS) for one channel
-void featurize(double *rms, double *crest, const int16_t *series, size_t len){
-  int16_t peak = 0;
-  *rms = 0;
-  for(int16_t i = len - 1; i >= 0; i--){
-    if(abs(series[i]) > peak){
-      peak = abs(series[i]);
+void featurize(double *rms, double *crest, const int16_t *series, size_t len)
+{
+    int16_t peak = 0;
+    *rms = 0;
+    for (int16_t i = len - 1; i >= 0; i--)
+    {
+        if (abs(series[i]) > peak)
+        {
+            peak = abs(series[i]);
+        }
+        *rms += (int32_t)series[i] * (int32_t)series[i];
     }
-    *rms += (int32_t) series[i] * (int32_t) series[i];
-  }
-  *rms /= len;
-  *rms = sqrt(*rms);
-  *crest = peak / *rms;
+    *rms /= len;
+    *rms = sqrt(*rms);
+    *crest = peak / *rms;
 }
 
-int benchmark_main(void){
-  double features[6];
-  double pred_featurized;
+int benchmark_main(void)
+{
+    double features[6];
+    volatile double pred_featurized;
 
-  featurize(&features[0], &features[1], acc_x3, sizeof(acc_x3) / sizeof(acc_x3[0]));
-  featurize(&features[2], &features[3], acc_y3, sizeof(acc_y3) / sizeof(acc_y3[0]));
-  featurize(&features[4], &features[5], acc_z3, sizeof(acc_z3) / sizeof(acc_z3[0]));
-  pred_featurized = score(features);
-  printf("Featurized prediction: %f\r\n", pred_featurized);
-  if(pred_featurized > 0){
-    printf("OK\r\n");
-  }else{
-    printf("NOK\r\n");
-  }
+    featurize(&features[0], &features[1], acc_x3, sizeof(acc_x3) / sizeof(acc_x3[0]));
+    featurize(&features[2], &features[3], acc_y3, sizeof(acc_y3) / sizeof(acc_y3[0]));
+    featurize(&features[4], &features[5], acc_z3, sizeof(acc_z3) / sizeof(acc_z3[0]));
+    pred_featurized = score(features);
+    printf("Featurized prediction: %f\r\n", printf_float(pred_featurized));
+    if (pred_featurized > 0)
+    {
+        printf("OK\r\n");
+    }
+    else
+    {
+        printf("NOK\r\n");
+    }
 
-  featurize(&features[0], &features[1], acc_x2, sizeof(acc_x2) / sizeof(acc_x2[0]));
-  featurize(&features[2], &features[3], acc_y2, sizeof(acc_y2) / sizeof(acc_y2[0]));
-  featurize(&features[4], &features[5], acc_z2, sizeof(acc_z2) / sizeof(acc_z2[0]));
-  pred_featurized = score(features);
-  printf("Featurized prediction: %f\r\n", pred_featurized);
-  if(pred_featurized > 0){
-    printf("OK\r\n");
-  }else{
-    printf("NOK\r\n");
-  }
+    featurize(&features[0], &features[1], acc_x2, sizeof(acc_x2) / sizeof(acc_x2[0]));
+    featurize(&features[2], &features[3], acc_y2, sizeof(acc_y2) / sizeof(acc_y2[0]));
+    featurize(&features[4], &features[5], acc_z2, sizeof(acc_z2) / sizeof(acc_z2[0]));
+    pred_featurized = score(features);
+    printf("Featurized prediction: %f\r\n", printf_float(pred_featurized));
+    if (pred_featurized > 0)
+    {
+        printf("OK\r\n");
+    }
+    else
+    {
+        printf("NOK\r\n");
+    }
 
-  featurize(&features[0], &features[1], acc_x1, sizeof(acc_x1) / sizeof(acc_x1[0]));
-  featurize(&features[2], &features[3], acc_y1, sizeof(acc_y1) / sizeof(acc_y1[0]));
-  featurize(&features[4], &features[5], acc_z1, sizeof(acc_z1) / sizeof(acc_z1[0]));
-  pred_featurized = score(features);
-  printf("Featurized prediction: %f\r\n", pred_featurized);
-  if(pred_featurized > 0){
-    printf("OK\r\n");
-  }else{
-    printf("NOK\r\n");
-  }
+    featurize(&features[0], &features[1], acc_x1, sizeof(acc_x1) / sizeof(acc_x1[0]));
+    featurize(&features[2], &features[3], acc_y1, sizeof(acc_y1) / sizeof(acc_y1[0]));
+    featurize(&features[4], &features[5], acc_z1, sizeof(acc_z1) / sizeof(acc_z1[0]));
+    pred_featurized = score(features);
+    printf("Featurized prediction: %f\r\n", printf_float(pred_featurized));
+    if (pred_featurized > 0)
+    {
+        printf("OK\r\n");
+    }
+    else
+    {
+        printf("NOK\r\n");
+    }
 
-  featurize(&features[0], &features[1], acc_x0, sizeof(acc_x0) / sizeof(acc_x0[0]));
-  featurize(&features[2], &features[3], acc_y0, sizeof(acc_y0) / sizeof(acc_y0[0]));
-  featurize(&features[4], &features[5], acc_z0, sizeof(acc_z0) / sizeof(acc_z0[0]));
-  pred_featurized = score(features);
-  printf("Featurized prediction: %f\r\n", pred_featurized);
-  if(pred_featurized > 0){
-    printf("OK\r\n");
-  }else{
-    printf("NOK\r\n");
-  }
-  return 0;
+    featurize(&features[0], &features[1], acc_x0, sizeof(acc_x0) / sizeof(acc_x0[0]));
+    featurize(&features[2], &features[3], acc_y0, sizeof(acc_y0) / sizeof(acc_y0[0]));
+    featurize(&features[4], &features[5], acc_z0, sizeof(acc_z0) / sizeof(acc_z0[0]));
+    pred_featurized = score(features);
+    printf("Featurized prediction: %f\r\n", printf_float(pred_featurized));
+    if (pred_featurized > 0)
+    {
+        printf("OK\r\n");
+    }
+    else
+    {
+        printf("NOK\r\n");
+    }
+    return 0;
 }
--- src/anomaly/svm.c
+++ src/anomaly/svm.c
@@ -1,17 +1,21 @@
 #include <math.h>
 #include <stdint.h>
 
-static double computeKernel(double *x, double v0, double v1, double v2, double v3, double v4, double v5){
-  double k = pow(v0 - x[0], 2.0) + pow(v1 - x[1], 2.0) + pow(v2 - x[2], 2.0) + pow(v3 - x[3], 2.0) + pow(v4 - x[4], 2.0) + pow(v5 - x[5], 2.0);
-  return exp(-0.001 * k);
+static double computeKernel(double *x, double v0, double v1, double v2, double v3, double v4, double v5)
+{
+    double k = pow(v0 - x[0], 2.0) + pow(v1 - x[1], 2.0) + pow(v2 - x[2], 2.0) + pow(v3 - x[3], 2.0) +
+               pow(v4 - x[4], 2.0) + pow(v5 - x[5], 2.0);
+
+    return exp(-0.001 * k);
 }
 
-double score(double *input){
-  double kernels[3] = {0};
-  kernels[0] = computeKernel(input, 389.255, 4.231, 111.702, 4.664, 1009.682, 1.313);
-  kernels[1] = computeKernel(input, 389.628, 4.266, 107.383, 4.079, 1009.527, 1.312);
-  kernels[2] = computeKernel(input, 394.161, 4.313, 108.048, 3.952, 1009.667, 1.319);
-  double score = -1.766 + (0.839 * kernels[0]) + (0.113 * kernels[1]) + (0.848 * kernels[2]);
-  return score;
-}
+double score(double *input)
+{
+    double kernels[3] = {0};
+    kernels[0] = computeKernel(input, 389.255, 4.231, 111.702, 4.664, 1009.682, 1.313);
+    kernels[1] = computeKernel(input, 389.628, 4.266, 107.383, 4.079, 1009.527, 1.312);
+    kernels[2] = computeKernel(input, 394.161, 4.313, 108.048, 3.952, 1009.667, 1.319);
+    double score = -1.766 + (0.839 * kernels[0]) + (0.113 * kernels[1]) + (0.848 * kernels[2]);
 
+    return score;
+}
