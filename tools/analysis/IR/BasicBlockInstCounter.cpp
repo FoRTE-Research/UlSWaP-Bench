a9ddb02a6acd36392137cbe0c15a7434485996d9
Daniel Chiba <danchiba@vt.edu>
2024-09-25 10:29:45 -0400

    Created LLVM IR analysis pass
    Created an out of tree LLVM pass to count the number of
    IR basic blocks and instructions.

--- tools/analysis/IR/BasicBlockInstCounter.cpp
+++ tools/analysis/IR/BasicBlockInstCounter.cpp
@@ -1,73 +1,54 @@
 #include "BasicBlockInstCounter.h"
-
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 
 using namespace llvm;
 
-static void printCountResult(raw_ostream &OutS, const BBICountResult &CountMap);
+static void printBasicBlockInstCounterResult(llvm::raw_ostream &, const ResultBBICounter &OC);
 
-//------------------------------------------------------------------------------
-// BasicBlockInstCounter Implementation
-//------------------------------------------------------------------------------
+llvm::AnalysisKey BasicBlockInstCounter::Key;
+
+BasicBlockInstCounter::Result BasicBlockInstCounter::run(llvm::Function &Func, llvm::FunctionAnalysisManager &)
+{
+    ResultBBICounter count = {&Func, 0, 0};
+    for (auto &BB : Func)
+    {
+        count.basicBlockCount++;
+        for (auto &Inst : BB)
+        {
+            count.instructionCount++;
+        }
+    }
+    return count;
+}
+
 PreservedAnalyses BasicBlockInstCounterPrinter::run(Function &Func, FunctionAnalysisManager &FAM)
 {
-    auto &Count = FAM.getResult<BasicBlockInstCounter>(Func);
+    auto &BBICount = FAM.getResult<BasicBlockInstCounter>(Func);
 
-    printCountResult(OS, Count);
+    printBasicBlockInstCounterResult(OS, BBICount);
     return PreservedAnalyses::all();
 }
 
-BasicBlockInstCounter::Result BasicBlockInstCounter::run(Function &Func, FunctionAnalysisManager &FAM)
-{
-    BBICountResult Count;
-    uint32_t TotalInsts = 0;
-    uint32_t TotalBBs = 0;
-
-    for (auto &BB : Func)
-    {
-        TotalBBs++;
-        for (auto &I : BB)
-        {
-            TotalInsts++;
-        }
-    }
-
-    Count = {&Func, TotalInsts, TotalBBs};
-    return Count;
-}
-
-//-----------------------------------------------------------------------------
-// New PM Registration
-//-----------------------------------------------------------------------------
 llvm::PassPluginLibraryInfo getBasicBlockInstCounterPluginInfo()
 {
     return {
-        LLVM_PLUGIN_API_VERSION, "BasicBlockInstCounter", LLVM_VERSION_STRING, [](PassBuilder &PB)
+        LLVM_PLUGIN_API_VERSION, "BasicBlockInstCounter", LLVM_VERSION_STRING,
+        [](PassBuilder &PB)
         {
-            // #1 REGISTRATION FOR "opt -passes=print<opcode-counter>"
+            // #1 REGISTRATION FOR "opt -passes=print-basic-block-inst-counter"
             // Register BasicBlockInstCounterPrinter so that it can be used when
             // specifying pass pipelines with `-passes=`.
-            PB.registerPipelineParsingCallback([&](StringRef Name, FunctionPassManager &FPM,
-                                                   ArrayRef<PassBuilder::PipelineElement>)
-                                               {
+            PB.registerPipelineParsingCallback(
+                [&](StringRef Name, FunctionPassManager &FPM,
+                    ArrayRef<PassBuilder::PipelineElement>)
+                {
                     if (Name == "print-basic-block-inst-counter")
                     {
                         FPM.addPass(BasicBlockInstCounterPrinter(llvm::errs()));
                         return true;
                     }
-                    return false; });
-            // #2 REGISTRATION FOR "-O{1|2|3|s}"
-            // Register BasicBlockInstCounterPrinter as a step of an existing pipeline.
-            // The insertion point is specified by using the
-            // 'registerVectorizerStartEPCallback' callback. To be more precise,
-            // using this callback means that BasicBlockInstCounterPrinter will be called
-            // whenever the vectoriser is used (i.e. when using '-O{1|2|3|s}'.
-            PB.registerVectorizerStartEPCallback(
-                [](llvm::FunctionPassManager &PM,
-                   llvm::OptimizationLevel Level)
-                {
-                    PM.addPass(BasicBlockInstCounterPrinter(llvm::errs()));
+                    return false;
                 });
             // #3 REGISTRATION FOR "FAM.getResult<BasicBlockInstCounter>(Func)"
             // Register BasicBlockInstCounter as an analysis pass. This is required so that
@@ -88,13 +69,9 @@ llvmGetPassPluginInfo()
     return getBasicBlockInstCounterPluginInfo();
 }
 
-//------------------------------------------------------------------------------
-// Helper functions
-//------------------------------------------------------------------------------
-static void printCountResult(raw_ostream &OutS, const BBICountResult &CountMap)
+static void printBasicBlockInstCounterResult(raw_ostream &OutS,
+                                             const ResultBBICounter &BBICount)
 {
-    OutS << "Function: " << CountMap.func->getName() << "\n";
-    OutS << "  Total Instructions: " << CountMap.totalInsts << "\n";
-    OutS << "  Total Basic Blocks: " << CountMap.totalBBs << "\n";
-    OutS << "\n";
+    OutS << BBICount.func->getName() << "," << BBICount.basicBlockCount << ","
+         << BBICount.instructionCount << "\n";
 }
