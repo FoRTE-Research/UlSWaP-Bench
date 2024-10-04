b6fdf1f609775073607debb6f45320415c6f5aac
Daniel Chiba <danchiba@vt.edu>
2024-10-04 16:54:48 -0400

    Reduce RSA rounds
    Reduce the number of rounds for RSA decryption in order to keep
    execution time reasonable.
    Fix warning in stringsearch.

--- src/rsa/rsa_test.h
+++ src/rsa/rsa_test.h
@@ -5,9 +5,16 @@
 #ifndef TINY_BIGNUM_RSA_RSA_TEST_H
 #define TINY_BIGNUM_RSA_RSA_TEST_H
 
+#include <stdint.h>
 #include "bn.h"
 
-void pow_mod_faster(struct bn* a, struct bn* b, struct bn* n, struct bn* res);
+#if HOST_TEST
+#define MAX_ROUNDS UINT32_MAX
+#else
+#define MAX_ROUNDS 16
+#endif
+
+void pow_mod_faster(struct bn* a, struct bn* b, struct bn* n, struct bn* res, uint32_t max_rounds);
 void rsa1024_encrypt(const char* public_key, uint64_t exponent, const char* plaintext_hex, char* output);
 void rsa1024_decrypt(const char* public_key, const char* private_key, const char* cipher, char* output);
 
