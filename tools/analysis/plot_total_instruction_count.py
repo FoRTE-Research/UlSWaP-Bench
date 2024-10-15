95dbf4c71281d6d489aad7cc370cb52b376abaec
Daniel Chiba <danchiba@vt.edu>
2024-10-15 19:53:12 -0400

    Grouped inst count plot
    Create a grouped bar graph with all 3 ISAs for total instruction
    counts. Currently hard-coded an hacky.

--- tools/analysis/plot_total_instruction_count.py
+++ tools/analysis/plot_total_instruction_count.py
@@ -1,5 +1,9 @@
 from utils import *
 
+ARM_CSV_FILE = 'output/arm_counts.csv'
+RISCV_DUMP_DIR = 'output/totcount'
+MSP430_DUMP_DIR = 'output/msp430_count'
+
 def get_runtime_count_msp430(file_path:str) -> int:
     with open(file_path, 'r') as f:
         lines = f.readlines()
@@ -36,10 +40,16 @@ def get_runtime_count_map_arm(csv_file:str) -> dict:
         parts = line.split(',')
         runtime_counts[parts[0]] = int(parts[1])
 
-    return runtime_counts
+    # rearrange the counts in the order of the benchmarks
+    bench_names = get_bench_names()
+    sorted_runtime_counts = {}
+    for bench in bench_names:
+        sorted_runtime_counts[bench] = runtime_counts[bench]
+
+    return sorted_runtime_counts
 
 
-def get_runtime_count(file_path:str) -> int:
+def get_runtime_count_riscv(file_path:str) -> int:
     with open(file_path, 'r') as f:
         lines = f.readlines()
 
@@ -48,7 +58,7 @@ def get_runtime_count(file_path:str) -> int:
     return count
 
 
-def get_runtime_count_map(directory:str) -> dict:
+def get_runtime_count_map_riscv(directory:str) -> dict:
     runtime_counts = {}
     bench_names = get_bench_names()
 
@@ -58,35 +68,64 @@ def get_runtime_count_map(directory:str) -> dict:
             print(f'No file found for {benchname}')
             runtime_counts[benchname] = 0
             continue
-        count = get_runtime_count(file_path)
+        count = get_runtime_count_riscv(file_path)
         runtime_counts[benchname] = count
 
     return runtime_counts
 
 
-def plot_total_instruction_count(runtime_counts_unsorted:dict[str, int], output_file:str=None) -> None:
+def plot_total_instruction_count(output_file:str=None) -> None:
     import matplotlib.pyplot as plt
+    import numpy as np
 
-    XTICK_SIZE_MAIN = 8
-    XTICK_SIZE_SEC = 8
-    YTICK_SIZE = 8
-    YAXIS_LABEL_SIZE = 8
-    BAR_TEXT_SIZE = 5
+    XTICK_SIZE_MAIN = 6
+    XTICK_SIZE_SEC = 6
+    YTICK_SIZE = 6
+    YAXIS_LABEL_SIZE = 6
+    BAR_TEXT_SIZE = 3.5
     FACTOR = 1000
 
+    archs = ['RISC-V', 'MSP430', 'ARM']
+    bench_list = get_bench_names()
+
     runtime_counts = {}
-    for bench in get_bench_names():
-        runtime_counts[bench] = runtime_counts_unsorted[bench]
+    for arch in archs:
+        if arch == 'RISC-V':
+            runtime_counts[arch] = g_runtime_counts_riscv.values()
+        elif arch == 'MSP430':
+            runtime_counts[arch] = g_runtime_counts_msp.values()
+        elif arch == 'ARM':
+            runtime_counts[arch] = g_runtime_counts_arm.values()
 
-    # Create a stacked bar chart of binary sizes for each benchmark
     fig, ax = plt.subplots()
-    fig.set_figwidth(6)
+    fig.set_figwidth(6.5)
+    fig.set_figheight(4)
+    plt.yscale('log')
 
-    for bench in runtime_counts.keys():
-        runtime_counts[bench] = round(runtime_counts[bench] / FACTOR)
+    x = np.arange(len(get_bench_names()))  # the label locations
+    width = 0.27  # the width of the bars
+    multiplier = 0
+    print(x)
 
-    ax.bar(runtime_counts.keys(), runtime_counts.values())
-    ax.set_ylabel(f'Total instructions ({FACTOR}s)', fontsize=YAXIS_LABEL_SIZE)
+    for arch, counts in runtime_counts.items():
+        print(arch, counts)
+        offset = width * multiplier
+        # smaller_counts = [round(count / FACTOR) for count in counts]
+        rects = ax.bar(x + offset, counts, width, label=arch)
+        ax.bar_label(rects, padding=1, fontsize=BAR_TEXT_SIZE, labels=[get_compact_num(count) for count in counts], rotation=90)
+        multiplier += 1
+
+    # plt.bar(x-0.2, runtime_counts['RISC-V'], width, label='RISC-V')
+    # plt.bar(x, runtime_counts['MSP430'], width, label='MSP430')
+    # plt.bar(x+0.2, runtime_counts['ARM'], width, label='ARM')
+
+    # for bench in runtime_counts.keys():
+    #     runtime_counts[bench] = round(runtime_counts[bench] / FACTOR)
+
+    # ax.bar(runtime_counts.keys(), runtime_counts.values())
+    ax.set_ylabel(f'Total instructions', fontsize=YAXIS_LABEL_SIZE)
+    ax.set_xticks(x + width * (len(archs) - 1) / 2, labels=get_bench_names())
+    # ax.set_xticks(x, labels=get_bench_names())
 
     sec = ax.secondary_xaxis(location=0)
     sec.set_xticks(get_label_xtick_positions(), labels=list('\n\n\n\n\n\n\n\n' + group for group in ALL_BENCHMARKS.keys()), weight='bold', size=XTICK_SIZE_SEC)
@@ -97,16 +136,18 @@ def plot_total_instruction_count(runtime_counts_unsorted:dict[str, int], output_
     sec2 = ax.secondary_xaxis(location=0)
     sec2.set_xticks(get_line_xticks(), labels=[])
     sec2.tick_params('x', length=65, width=1)
-    ax.set_xlim(-0.5, len(runtime_counts) - 0.5)
+    ax.set_xlim(-0.5, len(get_bench_names()))
+    # ax.set_xlim(0.5, len(get_bench_names()))
 
     # print total size on top of each bar
-    for bench, count in runtime_counts.items():
-        # ax.text(bench, count, str(count), rotation=90, ha='center', va='bottom', fontsize=BAR_TEXT_SIZE)
-        compact_count = get_compact_num(count)
-        ax.text(bench, count, compact_count, ha='center', va='bottom', fontsize=BAR_TEXT_SIZE)
+    # for bench, count in runtime_counts.items():
+    #     # ax.text(bench, count, str(count), rotation=90, ha='center', va='bottom', fontsize=BAR_TEXT_SIZE)
+    #     compact_count = get_compact_num(count)
+    #     ax.text(bench, count, compact_count, ha='center', va='bottom', fontsize=BAR_TEXT_SIZE)
 
     plt.xticks(rotation=90, ha='center', fontsize=XTICK_SIZE_MAIN)
     plt.yticks(fontsize=YTICK_SIZE)
+    plt.legend(loc='upper left', fontsize=5)
     ax.grid(axis='y')
     plt.tight_layout()
 
@@ -120,15 +161,24 @@ def plot_total_instruction_count(runtime_counts_unsorted:dict[str, int], output_
 
 def main():
     args = parse_args()
-    if args.msp430:
-        runtime_counts = get_runtime_count_map_msp430(args.dump_dir)
-    elif args.arm:
-        runtime_counts = get_runtime_count_map_arm(args.dump_file)
-    else:
-        runtime_counts = get_runtime_count_map(args.dump_dir)
 
-    print(runtime_counts)
-    plot_total_instruction_count(runtime_counts, args.plot_file)
+    global g_runtime_counts_riscv
+    global g_runtime_counts_msp
+    global g_runtime_counts_arm
+
+    g_runtime_counts_riscv = get_runtime_count_map_riscv(RISCV_DUMP_DIR)
+    g_runtime_counts_msp = get_runtime_count_map_msp430(MSP430_DUMP_DIR)
+    g_runtime_counts_arm = get_runtime_count_map_arm(ARM_CSV_FILE)
+
+    # if args.msp430:
+    #     runtime_counts = get_runtime_count_map_msp430(args.dump_dir)
+    # elif args.arm:
+    #     runtime_counts = get_runtime_count_map_arm(args.dump_file)
+    # else:
+    #     runtime_counts = get_runtime_count_map(args.dump_dir)
+
+    # print(runtime_counts)
+    plot_total_instruction_count(args.plot_file)
 
 
 if __name__ == '__main__':
