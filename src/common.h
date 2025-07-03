f78dc2a8fd23166690269a3e496a25265a4e0764
Daniel Chiba <danchiba@vt.edu>
2025-07-03 19:32:44 -0400

    Add checksum test functionality
    Add build option for CHECKSUM_TEST, which redirects all printf
    calls and keeps a running total of the argument values.

--- src/common.h
+++ src/common.h
@@ -2,23 +2,29 @@
 #define COMMON_H
 
 #include <stdint.h>
+#include <stdarg.h>
 
-#ifndef PICOLIBC_FLOAT_PRINTF_SCANF
-#define printf_float(_num) _num
+#if (!defined(PICOLIBC_FLOAT_PRINTF_SCANF) || (CHECKSUM_TEST))
+#undef printf_float
+#define printf_float(_num) (_num)
 #endif  // PICOLIBC_FLOAT_PRINTF_SCANF
-
-#if NO_PRINT
-#define printf(...)
-#endif  // NO_PRINT
 
 #ifndef HOST_TEST
 #define HOST_TEST 0
 #endif
+
+#if NO_PRINT
+#define printf(...)
+#elif CHECKSUM_TEST
+void printf_checksum(const char *fmt, ...);
+#define printf(fmt, ...) printf_checksum(fmt, ##__VA_ARGS__)
+#endif  // NO_PRINT
 
 #define TRUE  1
 #define FALSE 0
 
 void my_srand(uint32_t new_seed);
 uint32_t my_rand(void);
-
+uint32_t get_benchmark_checksum(void);
+// void test_checksum(void);        // Uncomment to test printf_checksum
 #endif  // COMMON_H
