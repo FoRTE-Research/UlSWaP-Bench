57910b1b058b426167a3419fbb24607cc2014804
Prakhar Sah <sprakhar@vt.edu>
2026-04-23 16:01:03 -0400

    add hw/msp430fr5994

---
+++ hw/msp430fr5994/config.cmake
@@ -0,0 +1,23 @@
+function (set_msp430_config)
+    message(STATUS "Setting MSP430 configuration")
+
+    set(PICOLIBC_ROOT $ENV{HOME}/picolibc/gcc-msp430)
+    set(MSP430_ROOT $ENV{HOME}/ti/msp430-gcc)
+
+    set(CC_PATH ${MSP430_ROOT}/bin)
+
+    set(CMAKE_C_COMPILER ${CC_PATH}/msp430-elf-gcc PARENT_SCOPE)
+    set(CMAKE_ASM_COMPILER ${CC_PATH}/msp430-elf-gcc PARENT_SCOPE)
+
+    set(GENERAL_FLAGS "-Wall;-Wextra;-Wpedantic;-Wshadow;-Wconversion;-Wpedantic;-Wstrict-prototypes;-fno-builtin;-ffreestanding;-fno-optimize-sibling-calls;-fno-builtin-fma;-ffp-contract=off;-ffunction-sections")
+    set(MSP430_FLAGS "-mlarge;-mdata-region=upper;-mmcu=msp430fr5994;-mhwmult=none;-specs=${PICOLIBC_ROOT}/picolibc.specs;-T${ARCH_DIR}/memmap.ld;-DCUSTOM_ARCH_STARTUP;-Wl,--gc-sections")
+
+    set(ARCH_OBJDUMP "${CC_PATH}/msp430-elf-objdump" PARENT_SCOPE)
+
+#    set(ARCH_LINK_DIRS "${MSP430_ROOT}/include" PARENT_SCOPE)
+    set(ARCH_INC_DIRS "${MSP430_ROOT}/include" PARENT_SCOPE)
+
+    set(ARCH_FLAGS "${GENERAL_FLAGS};${MSP430_FLAGS}" PARENT_SCOPE)
+    set(ARCH_LINK_FLAGS "${GENERAL_FLAGS};${MSP430_FLAGS}" PARENT_SCOPE)
+    set(ARCH_SOURCES "${ARCH_DIR}/supportFuncs.c;${ARCH_DIR}/vectors.S" PARENT_SCOPE)
+endfunction()
---
+++ hw/msp430fr5994/supportFuncs.c
@@ -0,0 +1,111 @@
+#include <msp430.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+
+static int sendByte(char byte, FILE *file);
+
+// Basic clock startup for 8MHz (max freq without FRAM wait states)
+void initClocks(void){
+  // Startup clock system with max DCO setting ~8MHz
+  CSCTL0_H = CSKEY_H;                     // Unlock CS registers
+  CSCTL1 = DCOFSEL_3 | DCORSEL;           // Set DCO to 8MHz
+  CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
+  CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers
+  CSCTL0_H = 0;                           // Lock CS registers
+}
+
+// Basic UART init for output
+void initUart(void){
+  // Configure USCI_A0 GPIOs
+  P2SEL1 |= (BIT0 | BIT1);
+  P2SEL0 &= ~(BIT0 | BIT1);       // USCI_A0 UART operation
+
+  // Configure USCI_A0 for UART mode
+  UCA0CTLW0 = UCSWRST;                    // Put eUSCI in reset
+  UCA0CTLW0 |= UCSSEL__SMCLK;             // CLK = SMCLK
+  // Baud Rate calculation
+  // 8000000/(16*9600) = 52.083
+  // Fractional portion = 0.083
+  // User's Guide Table 21-4: UCBRSx = 0x04
+  // UCBRFx = int ( (52.083-52)*16) = 1
+  UCA0BRW = 52;                           // 8000000/16/9600
+  UCA0MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
+  UCA0CTLW0 &= ~UCSWRST;                  // Initialize eUSCI
+  UCA0IE |= UCRXIE;                       // Enable USCI_A0 RX interrupt
+}
+
+// UART tx interrupt handler
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=EUSCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(EUSCI_A0_VECTOR))) USCI_A0_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+    switch(__even_in_range(UCA0IV, USCI_UART_UCTXCPTIFG))
+    {
+        case USCI_NONE: break;
+        case USCI_UART_UCRXIFG:
+            while(!(UCA0IFG&UCTXIFG));
+            __no_operation();
+            break;
+        case USCI_UART_UCTXIFG: break;
+        case USCI_UART_UCSTTIFG: break;
+        case USCI_UART_UCTXCPTIFG: break;
+        default: break;
+    }
+}
+
+void run_arch_startup(){
+
+  // Stop watchdog
+  WDTCTL = WDTPW | WDTHOLD;
+
+  // Peripheral init
+  initClocks();
+  initUart();
+
+  // Disable GPIO high-z
+  PM5CTL0 &= ~LOCKLPM5;
+
+  // Enable interrupts
+  __bis_SR_register(GIE);
+}
+
+void print_hexstring(uint32_t num){
+  for (uint32_t i = sizeof(num) * 8; i > 0; i -= 4){
+    uint8_t nibble = (num >> (i - 4)) & 0xF;
+    if (nibble > 9){
+      sendByte(nibble + 0x37, NULL);
+    }else{
+      sendByte(nibble + 0x30, NULL);
+    }
+  }
+}
+
+void hexstring(uint32_t num){
+  print_hexstring(num);
+  sendByte('\r', NULL);
+  sendByte('\n', NULL);
+}
+
+void hexstrings(uint32_t num){
+  print_hexstring(num);
+  sendByte(' ', NULL);
+}
+
+// Set up handles for picolibc stdout
+static int sendByte(char byte, FILE *file){
+  (void) file;
+  while(!(UCA0IFG&UCTXIFG));
+  UCA0TXBUF = byte;
+  return byte;
+}
+
+static FILE __stdio = FDEV_SETUP_STREAM(sendByte, NULL, NULL, _FDEV_SETUP_WRITE);
+FILE *const stdout = &__stdio;
+__strong_reference(stdout, stdin);
+__strong_reference(stdin, stderr);
