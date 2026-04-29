30f83c33ef9e49a8836aff78d70dc31fd2f33d41
Patrick Flynn <pflynn157@gmail.com>
2026-04-29 14:44:34 -0400

    RV32 HW Config

--- hw/rv32im/config.cmake
+++ hw/rv32im/config.cmake
@@ -2,19 +2,36 @@ function (set_rv32im_config)
     message(STATUS "Setting RISC-V 32 IM configuration")
 
     set(PICOLIBC_ROOT $ENV{HOME}/picolibc/rv32im)
-
-    set(CMAKE_C_COMPILER riscv64-unknown-elf-gcc PARENT_SCOPE)
-    set(CMAKE_ASM_COMPILER riscv64-unknown-elf-gcc PARENT_SCOPE)
     
-    set(GENERAL_FLAGS "-nostartfiles;-nostdlib;-ffreestanding;-march=rv32im;-mabi=ilp32;-O2;-T${ARCH_DIR}/memmap.ld")
+    set(COMPILER "DEFAULT" CACHE PATH "The compiler to build these benchmarks")
+    if (COMPILER STREQUAL "DEFAULT")
+        message(STATUS "Using default RISC-V compiler")
+        set(CMAKE_C_COMPILER riscv64-unknown-elf-gcc PARENT_SCOPE)
+        set(CMAKE_ASM_COMPILER riscv64-unknown-elf-gcc PARENT_SCOPE)
+    else()
+        message(STATUS "Using custom compiler: ${COMPILER}")
+        set(CMAKE_C_COMPILER "${COMPILER}" PARENT_SCOPE)
+        set(CMAKE_ASM_COMPILER "${COMPILER}" PARENT_SCOPE)
+    endif()
+    
+    set(LINKER_SCRIPT "-T${ARCH_DIR}/memmap.ld")
+    if (CUSTOM_MEMMAP)
+        set(LINKER_SCRIPT "-T${CUSTOM_MEMMAP}")
+    endif()
+    
+    set(GENERAL_FLAGS "-nostartfiles;-nostdlib;-ffreestanding;-march=rv32im;-mabi=ilp32;-O2;${LINKER_SCRIPT}")
+    
+    separate_arguments(OPT_FLAGS NATIVE_COMMAND "${OPT_FLAGS}")
+    separate_arguments(LINK_DIRS NATIVE_COMMAND "${LINK_DIRS}")
+    separate_arguments(LINK_LIBS NATIVE_COMMAND "${LINK_LIBS}")
 
-    set(ARCH_FLAGS "${GENERAL_FLAGS}" PARENT_SCOPE)
-    set(ARCH_LINK_FLAGS "${GENERAL_FLAGS}" PARENT_SCOPE)
+    set(ARCH_FLAGS "${GENERAL_FLAGS};${OPT_FLAGS}" PARENT_SCOPE)
+    set(ARCH_LINK_FLAGS "${GENERAL_FLAGS};${OPT_FLAGS}" PARENT_SCOPE)
     set(ARCH_SOURCES "${ARCH_DIR}/supportFuncs.c;${ARCH_DIR}/vectors.S" PARENT_SCOPE)
     
     set(ARCH_INC_DIRS "${PICOLIBC_ROOT}/include" PARENT_SCOPE)
-    set(ARCH_LINK_DIRS "/usr/lib/gcc/riscv64-unknown-elf/13.2.0/rv32im/ilp32;${PICOLIBC_ROOT}/lib" PARENT_SCOPE)
-    set(ARCH_LIBS "gcc" PARENT_SCOPE)
+    set(ARCH_LINK_DIRS "/usr/lib/gcc/riscv64-unknown-elf/13.2.0/rv32im/ilp32;${PICOLIBC_ROOT}/lib;${LINK_DIRS}" PARENT_SCOPE)
+    set(ARCH_LIBS "gcc;${LINK_LIBS}" PARENT_SCOPE)
     
     set(ARCH_OBJDUMP riscv64-unknown-elf-objdump PARENT_SCOPE)
     
