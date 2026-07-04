0a838fefc97b3ed36c2b9fc7e094a641bfc537fe
Daniel Chiba <danchiba@vt.edu>
2026-07-04 03:25:50 -0400

    Change random implementation
    Change my_rand to stop using multiplication.
    Update referecne outputs for bitcount and fft.

--- src/common.c
+++ src/common.c
@@ -10,7 +10,7 @@ void my_srand(uint32_t new_seed)
 
 uint32_t my_rand(void)
 {
-    _myrand_seed = (uint32_t)(1103515245 * _myrand_seed + 12345);
+    _myrand_seed = (_myrand_seed >> 1) ^
+                   (uint32_t)(-(uint32_t)(_myrand_seed & UINT32_C(1)) & UINT32_C(0xD0000001));
     return _myrand_seed;
 }
-
