6324667976fbe4e24b5f017d68137f07f9805432
Daniel Chiba <danchiba@vt.edu>
2026-07-13 20:31:09 -0400

    Update total instruction count plot
    Consolidate fetching of instruction counts into single common
    function for all 3 architectures (assumes simulators print
    counts in a similar format).
    Modify plot dimensions and color scheme.
    Add ability to cache instruction counts.

--- tools/analysis/plot_total_instruction_count.py
+++ tools/analysis/plot_total_instruction_count.py
@@ -1,147 +1,97 @@
+import json
 from utils import *
+from plot_utils import *
 
-ARM_CSV_FILE = 'output/arm_counts.csv'
-RISCV_DUMP_DIR = 'output/totcount'
-MSP430_DUMP_DIR = 'output/msp430_counts2'
+COUNT_LABEL = 'Instruction Count: '
 
-def get_runtime_count_msp430(file_path:str) -> int:
+
+def get_runtime_count(file_path:str) -> int:
     with open(file_path, 'r') as f:
-        lines = f.readlines()
+        lines = f.readlines(-10)
+        for line in reversed(lines):
+            if COUNT_LABEL in line:
+                count = int(line.split(COUNT_LABEL)[1].strip())
+                return count
 
-    count = 0
-    for line in lines[1:]:
-        count += int(line.split(',')[1])
-
-    return count
+    raise ValueError(f'No line with {COUNT_LABEL} found in {file_path}')
 
 
-def get_runtime_count_map_msp430(directory:str) -> dict:
+def get_runtime_count_map(arch:str, arch_counts_dir:str) -> dict[str, int]:
     runtime_counts = {}
     bench_names = get_bench_names()
 
     for benchname in bench_names:
-        file_path = os.path.join(directory, f'{benchname}.csv')
+        file_path = os.path.join(arch_counts_dir, f'{benchname}')
         if not os.path.exists(file_path):
             print(f'No file found for {benchname}')
             runtime_counts[benchname] = 0
             continue
-        count = get_runtime_count_msp430(file_path)
+        count = get_runtime_count(file_path)
         runtime_counts[benchname] = count
 
     return runtime_counts
 
 
-def get_runtime_count_map_arm(csv_file:str) -> dict:
-    runtime_counts = {}
-    with open(csv_file, 'r') as f:
-        lines = f.readlines()
+def get_runtime_count_maps(riscv_dir:str, msp430_dir:str, arm_dir:str) -> dict[str, dict[str, int]]:
+    runtime_count_maps = {}
+    for arch in ARCHITECTURES:
+        if arch == 'riscv':
+            runtime_count_maps[arch] = get_runtime_count_map(arch, riscv_dir)
+        elif arch == 'msp430':
+            runtime_count_maps[arch] = get_runtime_count_map(arch, msp430_dir)
+        elif arch == 'arm':
+            runtime_count_maps[arch] = get_runtime_count_map(arch, arm_dir)
 
-    for line in lines[1:]:
-        parts = line.split(',')
-        runtime_counts[parts[0]] = int(parts[1])
-
-    # rearrange the counts in the order of the benchmarks
-    bench_names = get_bench_names()
-    sorted_runtime_counts = {}
-    for bench in bench_names:
-        sorted_runtime_counts[bench] = runtime_counts[bench]
-
-    return sorted_runtime_counts
+    return runtime_count_maps
 
 
-def get_runtime_count_riscv(file_path:str) -> int:
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-
-    final_count_line = lines[-2]
-    count = int(final_count_line.split()[0])
-    return count
-
-
-def get_runtime_count_map_riscv(directory:str) -> dict:
-    runtime_counts = {}
-    bench_names = get_bench_names()
-
-    for benchname in bench_names:
-        file_path = os.path.join(directory, f'{benchname}')
-        if not os.path.exists(file_path):
-            print(f'No file found for {benchname}')
-            runtime_counts[benchname] = 0
-            continue
-        count = get_runtime_count_riscv(file_path)
-        runtime_counts[benchname] = count
-
-    return runtime_counts
-
-
-def plot_total_instruction_count(output_file:str=None) -> None:
-    import matplotlib as mpl
-    import matplotlib.pyplot as plt
-    import numpy as np
-
-    FONT_SIZE = 8
-    XTICK_SIZE_MAIN = FONT_SIZE
-    XTICK_SIZE_SEC = FONT_SIZE
-    YTICK_SIZE = FONT_SIZE
-    YAXIS_LABEL_SIZE = FONT_SIZE
-    BAR_TEXT_SIZE = 4
-    FACTOR = 1000
-
-    BAR_LINE_WIDTH = 1
+def plot_total_instruction_count(counts:dict[str, dict[str, int]], output_file:str|None=None) -> None:
+    FONT_SIZE = 7.5
+    FIGURE_HEIGHT = 3
+    SEC_GAP_LINES = 7
+    SEC_TICK_LENGTH = 73
+    LEGEND_LABEL_SPACING = 0.35
+    TICK_PADDING = 2
+    BAR_LINE_WIDTH = 0.8
     HATCH_LINE_WIDTH = 0.5
+    BAR_WIDTH = 0.27
+
     mpl.rcParams['hatch.linewidth'] = HATCH_LINE_WIDTH
 
-    archs = ['RISC-V', 'MSP430', 'ARM']
-    bench_list = get_bench_names()
-
-    runtime_counts = {}
-    for arch in archs:
-        if arch == 'RISC-V':
-            runtime_counts[arch] = g_runtime_counts_riscv.values()
-        elif arch == 'MSP430':
-            runtime_counts[arch] = g_runtime_counts_msp.values()
-        elif arch == 'ARM':
-            runtime_counts[arch] = g_runtime_counts_arm.values()
-
-    fig, ax = plt.subplots()
-    fig.set_figwidth(6.5)
-    fig.set_figheight(4)
+    fig, ax = plt.subplots(constrained_layout=True)
+    fig.set_figwidth(PAGEWIDTH)
+    fig.set_figheight(FIGURE_HEIGHT)
     plt.yscale('log')
 
     x = np.arange(len(get_bench_names()))  # the label locations
-    width = 0.27  # the width of the bars
     multiplier = 0
 
-    for arch, counts in runtime_counts.items():
-        offset = width * multiplier
-        rects = ax.bar(x + offset, counts, width, label=arch, color=ARCH_COLORS[multiplier], hatch=ARCH_HATCHES[multiplier], edgecolor='black', linewidth=BAR_LINE_WIDTH)
+    for arch, count_map in counts.items():
+        offset = BAR_WIDTH * multiplier
+        rects = ax.bar(x + offset, list(count_map.values()), BAR_WIDTH, label=ARCH_LABELS[arch], color=ARCH_COLORS[multiplier], hatch=ARCH_HATCHES[multiplier], edgecolor='black', linewidth=BAR_LINE_WIDTH)
         multiplier += 1
 
-    ax.set_ylabel(f'Total instructions executed', fontsize=YAXIS_LABEL_SIZE, labelpad=1)
-    ax.set_xticks(x + width * (len(archs) - 1) / 2, labels=get_bench_names())
+    ax.set_ylabel(f'Total instructions executed', fontsize=FONT_SIZE, labelpad=TICK_PADDING)
+    ax.set_xticks(x + BAR_WIDTH * (len(ARCHITECTURES) - 1) / 2, labels=get_bench_names())
 
     # Category labels
     sec = ax.secondary_xaxis(location=0)
-    sec.set_xticks(get_label_xtick_positions(), labels=list('\n\n\n\n\n\n\n\n' + group for group in ALL_BENCHMARKS.keys()), weight='bold', size=XTICK_SIZE_SEC)
+    sec.set_xticks(get_label_xtick_positions(), labels=list('\n' * SEC_GAP_LINES + group for group in ALL_BENCHMARKS.keys()), weight='bold', size=FONT_SIZE)
     sec.tick_params('x', length=0)
 
     # Lines between the categories:
     sec2 = ax.secondary_xaxis(location=0)
     sec2.set_xticks(get_line_xticks(), labels=[])
-    sec2.tick_params('x', length=82, width=1)
+    sec2.tick_params('x', length=SEC_TICK_LENGTH, width=1)
     ax.set_xlim(-0.5, len(get_bench_names()))
 
-    # print total size on top of each bar
-    # for bench, count in runtime_counts.items():
-    #     # ax.text(bench, count, str(count), rotation=90, ha='center', va='bottom', fontsize=BAR_TEXT_SIZE)
-    #     compact_count = get_compact_num(count)
-    #     ax.text(bench, count, compact_count, ha='center', va='bottom', fontsize=BAR_TEXT_SIZE)
-
-    plt.xticks(rotation=90, ha='center', fontsize=XTICK_SIZE_MAIN)
-    plt.yticks(fontsize=YTICK_SIZE)
-    plt.legend(fontsize=FONT_SIZE, bbox_to_anchor=(0.23, 1))
+    plt.xticks(rotation=90, ha='center', fontsize=FONT_SIZE)
+    plt.yticks(fontsize=FONT_SIZE)
+    plt.legend(fontsize=FONT_SIZE, bbox_to_anchor=(0.23, 1), labelspacing=LEGEND_LABEL_SPACING)
     ax.grid(axis='y')
-    plt.tight_layout()
+
+    # Reduce padding between tick and tick label
+    ax.tick_params(axis='both', which='major', pad=TICK_PADDING)
 
     if output_file is not None:
         plt.savefig(output_file)
@@ -154,7 +104,10 @@ def plot_total_instruction_count(output_file:str=None) -> None:
 help_msg = '''
 This script retrieves and plots the total instruction count for each benchmark for all 3 architectures
 as a grouped bar chart.
-The input for RISC-V and MSP430 are directories and for ARM is a CSV file. The formats are architecture specific.
+The inputs are directories with filenames matching the names of the benchmarks, and each file containing one
+line towards the end of the file with the total instruction count preceded by the label "Total instructions:".
+All other lines in the files are ignored.
+Inputs are provided using arch-specific arguments instead of -i/--input.
 If no output file is specified, the plot will be displayed.
 '''
 
@@ -162,18 +115,30 @@ If no output file is specified, the plot will be displayed.
 def main():
     parent_parser = get_parent_parser(False, False)
     parser = argparse.ArgumentParser(parents=[parent_parser], description=help_msg)
+    parser.add_argument('--riscv', required=True, help='Path to directory with RISC-V instruction count dumps')
+    parser.add_argument('--msp430', required=True, help='Path to directory with MSP430 instruction count dumps')
+    parser.add_argument('--arm', required=True, help='Path to directory with ARM instruction count dumps')
+    parser.add_argument('--cache', help='Path to the JSON file with cached instruction counts')
+    parser.add_argument('--refresh', action='store_true', help='Do not use the cached counts; refresh the existing cache.')
     args = parser.parse_args()
+    cache_file = args.cache
     output_file = args.output
+    refresh = args.refresh
+    counts = None
 
-    global g_runtime_counts_riscv
-    global g_runtime_counts_msp
-    global g_runtime_counts_arm
+    if refresh or cache_file is None:
+        counts = get_runtime_count_maps(args.riscv, args.msp430, args.arm)
+        if cache_file is not None:
+            with open(cache_file, 'w') as f:
+                json.dump(counts, f, indent=4)
+    else:
+        if cache_file is not None:
+            with open(cache_file, 'r') as f:
+                counts = json.load(f)
 
-    g_runtime_counts_riscv = get_runtime_count_map_riscv(RISCV_DUMP_DIR)
-    g_runtime_counts_msp = get_runtime_count_map_msp430(MSP430_DUMP_DIR)
-    g_runtime_counts_arm = get_runtime_count_map_arm(ARM_CSV_FILE)
-
-    plot_total_instruction_count(output_file)
+    assert(counts is not None)
+    assert('riscv' in counts and 'msp430' in counts and 'arm' in counts)
+    plot_total_instruction_count(counts, output_file)
 
 
 if __name__ == '__main__':
